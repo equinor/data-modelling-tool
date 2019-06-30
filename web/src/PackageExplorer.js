f8bb31058e535c062a0ad7a63222c697385431a3
Eirik Ola Aksnes <eoaksnes@gmail.com>
2019-06-30 14:27:56 +0200

    inital version

---
+++ web/src/PackageExplorer.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Tree from './components/Tree';
+
+const StyledFileExplorer = styled.div`
+  width: 800px;
+  max-width: 100%;
+  margin: 0 auto;
+  display: flex;  
+`;
+
+const TreeWrapper = styled.div`
+  width: 250px;
+`;
+
+const data = {
+  '/root': {
+    path: '/root',
+    type: 'folder',
+    isRoot: true,
+    children: ['/root/david', '/root/jslancer'],
+  },
+  '/root/david': {
+    path: '/root/david',
+    type: 'folder',
+    children: ['/root/david/readme.md'],
+  },
+  '/root/david/readme.md': {
+    path: '/root/david/readme.md',
+    type: 'file',
+    content: 'Thanks for reading me me. But there is nothing here.'
+  },
+  '/root/jslancer': {
+    path: '/root/jslancer',
+    type: 'folder',
+    children: ['/root/jslancer/projects', '/root/jslancer/vblogs'],
+  },
+  '/root/jslancer/projects': {
+    path: '/root/jslancer/projects',
+    type: 'folder',
+    children: ['/root/jslancer/projects/treeview'],
+  },
+  '/root/jslancer/projects/treeview': {
+    path: '/root/jslancer/projects/treeview',
+    type: 'folder',
+    children: [],
+  },
+  '/root/jslancer/vblogs': {
+    path: '/root/jslancer/vblogs',
+    type: 'folder',
+    children: [],
+  },
+};
+
+export default class FileExplorer extends Component { 
+  state = {
+    selectedFile: null,
+  };
+
+  onSelect = (file) => this.setState({ selectedFile: file });
+
+  render() {
+    const { selectedFile } = this.state;
+
+    return (
+      <StyledFileExplorer>
+        <TreeWrapper>
+          <Tree 
+          data={data} 
+          onSelect={this.onSelect} />
+        </TreeWrapper>
+        <div>
+          { selectedFile && selectedFile.type === 'file' && selectedFile.content }
+        </div>
+      </StyledFileExplorer>
+    )
+  }
+}
---
+++ web/src/components/Tree.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import values from 'lodash/values';
+import PropTypes from 'prop-types';
+
+import TreeNode from './TreeNode';
+
+export default class Tree extends Component {
+
+  state = {
+    nodes: [],
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodes: props.data
+    }
+  }
+
+
+  getRootNodes = () => {
+    const { nodes } = this.state;
+    return values(nodes).filter(node => node.isRoot === true);
+  }
+
+  getChildNodes = (node) => {
+    const { nodes } = this.state;
+    if (!node.children) return [];
+    return node.children.map(path => nodes[path]);
+  }  
+
+  onToggle = (node) => {
+    const { nodes } = this.state;
+    nodes[node.path].isOpen = !node.isOpen;
+    this.setState({ nodes });
+  }
+
+  onNodeSelect = node => {
+    const { onSelect } = this.props;
+    onSelect(node);
+  }
+
+  render() {
+    const rootNodes = this.getRootNodes();
+    return (
+      <div>
+        { rootNodes.map(node => (
+          <TreeNode 
+            node={node}
+            getChildNodes={this.getChildNodes}
+            onToggle={this.onToggle}
+            onNodeSelect={this.onNodeSelect}
+          />
+        ))}
+      </div>
+    )
+  }
+}
+
+Tree.propTypes = {
+  onSelect: PropTypes.func.isRequired,
+};
