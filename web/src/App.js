f5b31b38bc0460b3da7cc2ccd815db3d6be1f823
Eirik Ola Aksnes <eoaksnes@gmail.com>
2019-07-02 12:06:01 +0200

    feat: react-jsonschema-form

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import PackageExplorer from './PackageExplorer'
+import React from 'react';
+import PackageExplorer from './PackageExplorer';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Package from './pages/Package';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <PackageExplorer />
-    </div>
-  )
+    <Router>
+        <div>
+          <PackageExplorer />
+          <Route exact path="/" component={Home} />
+          <Route path="/package" component={Package} />
+        </div>
+    </Router>
+  );
 }
 
 export default App
---
+++ web/src/pages/Package.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+import Form from "react-jsonschema-form";
+
+const schema = {
+    "$schema": "http://json-schema.org/draft-04/schema",
+    "title": "Package",
+    "description": "Template for SIMOS-type Packages",
+    "properties": {
+        "name": {
+          "description": "The name of the Package",
+          "type": "string"
+       }
+    },
+    "required": [
+      "name"
+    ]    
+};
+
+const log = (type) => console.log.bind(console, type);
+
+const PackageForm = () => {
+    return (
+        <Form schema={schema}
+            onChange={log("changed")}
+            onSubmit={log("submitted")}
+            onError={log("errors")} />
+    )
+}
+
+export default PackageForm;
