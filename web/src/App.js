adedbe1967ebf02d46a618d06ddd8f4ddfb5baa7
Eirik Ola Aksnes <eoaksnes@gmail.com>
2019-07-02 12:36:03 +0200

    feat: react-styled-flexboxgrid

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import PackageExplorer from './PackageExplorer';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Package from './pages/Package';
+import React from 'react'
+import PackageExplorer from './PackageExplorer'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Package from './pages/Package'
+import { Grid, Col, Row } from 'react-styled-flexboxgrid'
 
 function Home() {
   return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
+    <Grid>
+      <Row>
+        <Col xs={12} md={4}>
+          <PackageExplorer />
+        </Col>
+        <Col xs={12} md={8}>
+          Something else....
+        </Col>
+      </Row>
+    </Grid>
+  )
 }
 
 function App() {
   return (
     <Router>
-        <div>
-          <PackageExplorer />
-          <Route exact path="/" component={Home} />
-          <Route path="/package" component={Package} />
-        </div>
+      <Route exact path="/" component={Home} />
+      <Route path="/package" component={Package} />
     </Router>
-  );
+  )
 }
 
 export default App
