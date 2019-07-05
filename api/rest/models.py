96dfd0da9413cdaa663712b4d54a3b84065ca7ce
cbuv <cbuv@equinor.com>
2019-07-05 12:18:43 +0200

    fix routing
    Signed-off-by: cbuv <cbuv@equinor.com>

--- api/rest/models.py
+++ api/rest/models.py
@@ -3,7 +3,7 @@ import json
 from flask import jsonify
 
 
-@bp.route('/<path:path>', methods=['GET'])
+@bp.route('/api/<path:path>', methods=['GET'])
 def api(path):
     with open(f'/code/models/{path}') as json_file:
         return jsonify(json.load(json_file))
