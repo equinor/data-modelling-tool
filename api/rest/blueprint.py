2a7a0552752e98c9cc2ee229701c1adeeac6929e
Stig Ofstad <stigofstad@gmail.com>
2019-07-11 15:36:09 +0200

    hotfix: plural collecion names

--- api/rest/blueprint.py
+++ api/rest/blueprint.py
@@ -7,7 +7,7 @@ from services.database import db
 class Blueprint(Resource):
     @staticmethod
     def get(path):
-        document = db.blueprint.find_one({"_id": path})
+        document = db.blueprints.find_one({"_id": path})
         if not document:
             return abort(404)
         return jsonify(document)
@@ -17,7 +17,7 @@ class Blueprint(Resource):
         data = request.get_json()
         data['_id'] = path
         try:
-            db.blueprint.replace_one({'_id': path}, data, upsert=True)
+            db.blueprints.replace_one({'_id': path}, data, upsert=True)
             return path
         except Exception as e:
             abort(500, f"Something went wrong: {e}")
--- api/rest/template.py
+++ api/rest/template.py
@@ -7,7 +7,7 @@ from services.database import db
 class Template(Resource):
     @staticmethod
     def get(path):
-        document = db.template.find_one({"_id": path})
+        document = db.templates.find_one({"_id": path})
         if not document:
             return abort(404)
         return jsonify(document)
@@ -17,7 +17,7 @@ class Template(Resource):
         data = request.get_json()
         data['_id'] = path
         try:
-            db.template.replace_one({'_id': path}, data, upsert=True)
+            db.templates.replace_one({'_id': path}, data, upsert=True)
             return path
         except Exception as e:
             abort(500, f"Something went wrong: {e}")
