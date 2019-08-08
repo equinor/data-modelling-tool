31aae884bfa400410eb08bba2ef6a8bc8704fa2a
Stig Ofstad <stigofstad@gmail.com>
2019-08-08 14:39:58 +0200

    add azure mongo uri

--- api/config.py
+++ api/config.py
@@ -4,6 +4,8 @@ import os
 class Config:
     REMOTE_DEBUG = 0
     FLASK_DEBUG = os.getenv('FLASK_DEBUG', 0)
+    ENVIRONMENT = os.getenv('ENVIRONMENT', '')
     MONGO_USERNAME = os.getenv('MONGO_INITDB_ROOT_USERNAME', 'maf')
     MONGO_PASSWORD = os.getenv('MONGO_INITDB_ROOT_PASSWORD', 'maf')
+    MONGO_URI = os.getenv('MONGO_AZURE_URI', '')
     MONGO_DB = os.getenv('MONGO_INITDB_DATABASE', 'db')
