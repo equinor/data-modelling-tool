11b86a677124d62e1d333515ea792dbc25753641
Eirik Ola Aksnes <eoaksnes@gmail.com>
2019-08-09 10:34:42 +0200

    fix db connection and remove unused package properties

--- api/services/database.py
+++ api/services/database.py
@@ -2,12 +2,12 @@ from pymongo import MongoClient
 
 from config import Config
 
-client = MongoClient(Config.MONGO_URI)
-
 if Config.ENVIRONMENT == 'local':
     client = MongoClient('db',
                          username=Config.MONGO_USERNAME,
                          password=Config.MONGO_PASSWORD,
                          authMechanism='SCRAM-SHA-256')
+else:
+    client = MongoClient(Config.MONGO_URI)
 
 db = client[Config.MONGO_DB]
