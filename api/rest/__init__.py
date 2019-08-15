1804bdf449d64a603cb3f8b519a1243467f4d80b
Stig Ofstad <stigofstad@gmail.com>
2019-08-15 15:04:57 +0200

    simple data-source api

--- api/rest/__init__.py
+++ api/rest/__init__.py
@@ -5,10 +5,14 @@ from rest.blueprint_to_json_schema import BlueprintToJsonSchema
 from rest.entity import Entity
 from rest.template import Template
 from rest.index import Index
+from rest.data_sources import DataSources
 
 
 def create_api(app):
     api = Api(app)
+    api.add_resource(DataSources, '/api/data-sources')
+
+    # TODO: Legacy...
     api.add_resource(Template, '/api/templates/<path:path>')
     api.add_resource(Blueprint, '/api/blueprints/<path:path>')
     api.add_resource(BlueprintToJsonSchema, '/api/blueprints/<path:path>/json-schema')
---
+++ api/utils/logging.py
@@ -0,0 +1,6 @@
+import logging
+
+from config import Config
+
+logging.basicConfig(level=Config.LOGGER_LEVEL, format='%(levelname)s:%(asctime)s %(message)s')
+logger = logging.getLogger()
