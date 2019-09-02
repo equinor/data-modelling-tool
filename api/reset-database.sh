8545cdc8f54c0eddeb5b42a4f343c737ffdb88c6
Sindre Nistad <snis@equinor.com>
2019-09-02 11:46:06 +0200

    fix: Schemas are imported

--- api/reset-database.sh
+++ api/reset-database.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 set -eu
 
+mkdir -p /code/schemas/documents/entities
+
 flask nuke-db
 flask init-import
+
+if [ "$ENVIRONMENT" = 'local' ]; then
+    echo "Populating database"
+    for dataSource in /code/schemas/data-sources/"${ENVIRONMENT}"-mongo-*.json ; do
+      flask import-data-source "$dataSource"
+    done
+fi
