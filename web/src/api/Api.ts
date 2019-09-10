60f21e377ae09ae8be27ad9bfbcc239b7a03d337
Sindre Nistad <snis@equinor.com>
2019-09-10 17:26:24 +0200

    web: feat: Added local, persistent workspace. Closes #126

--- web/src/api/Api.ts
+++ web/src/api/Api.ts
@@ -1,3 +1,95 @@
+import axios from 'axios'
+import values from 'lodash/values'
+
+import Workspace from '../util/localWorkspace'
+
+function isLocal(datasource: Datasource): boolean {
+  return datasource.id === 'local'
+}
+
+export interface IndexRequestBody {
+  id: string
+  content: any
+}
+
+export interface PostIndexProp extends ApiPostProp {
+  datasource: Datasource
+  index: IndexRequestBody[]
+}
+
+export interface ApiPostProp {
+  onSuccess?: () => void
+  onError?: (error: any) => void
+}
+
+export abstract class BaseApi {
+  public readonly dmtApi: DmtApi
+  protected workspace: Workspace
+
+  protected constructor() {
+    this.dmtApi = new DmtApi()
+    this.workspace = new Workspace()
+  }
+
+  abstract async post(props: ApiPostProp): Promise<any>
+  abstract async get(props: any): Promise<any>
+}
+
+export class IndexApi extends BaseApi {
+  public constructor() {
+    super()
+  }
+
+  async post({
+    datasource,
+    index,
+    onSuccess = () => {},
+    onError = e => {},
+  }: PostIndexProp) {
+    const url = this.dmtApi.indexPost(datasource.id)
+    axios
+      .post(url, index)
+      .then(res => {
+        if (isLocal(datasource)) {
+          this.workspace.setItem(url, res.data)
+        } else {
+          console.log('We have an index! ', res)
+        }
+        onSuccess()
+      })
+      .catch(e => {
+        onError(e)
+      })
+  }
+
+  async get(datasource: Datasource) {
+    const url = this.dmtApi.indexGet(datasource.id)
+    try {
+      const res = !isLocal(datasource)
+        ? (await axios(url)).data
+        : this.workspace.getItem(url)
+      const nodes = values(res)
+
+      const documents = nodes
+        .map(node => {
+          return {
+            ...node,
+            nodeId: node.id,
+            isOpen: false,
+          }
+        })
+        .reduce((obj, item) => {
+          obj[item.nodeId] = item
+          return obj
+        }, {})
+      return documents
+    } catch (err) {
+      console.error(err)
+    }
+    return {}
+  }
+}
+
 export class DmtApi {
   dataSourcesGet(dataSourceType: DataSourceType): string {
     return `/api/data-sources/${dataSourceType}`
