a68cf49f956fbe994be36d7369bfa26aad910c97
Eirik Ola Aksnes <eoaksnes@gmail.com>
2019-09-18 18:58:45 +0200

    add request and response handling

--- api/core/rest/Explorer.py
+++ api/core/rest/Explorer.py
@@ -3,12 +3,19 @@ from flask import Blueprint, Response, request
 from classes.data_source import DataSource
 from core.serializers.document_json_serializer import DocumentSerializer
 from core.repository.repository_factory import get_repository, RepositoryType
-from core.use_case.add_file_to_package_use_case import AddFileToPackageUseCase
+from core.use_case.add_file_to_package_use_case import AddFileToPackageUseCase, AddFileToPackageRequestObject
 from core.use_case.add_package_to_package_use_case import AddPackageToPackageUseCase
-from core.domain.document import Document
 from core.domain.package import SubPackage
+from core.shared import response_object as res
 
 blueprint = Blueprint("explorer", __name__)
+
+STATUS_CODES = {
+    res.ResponseSuccess.SUCCESS: 200,
+    res.ResponseFailure.RESOURCE_ERROR: 404,
+    res.ResponseFailure.PARAMETERS_ERROR: 400,
+    res.ResponseFailure.SYSTEM_ERROR: 500,
+}
 
 
 @blueprint.route("/api/explorer/<string:data_source_id>/add-file", methods=["POST"])
@@ -21,11 +28,14 @@ def add_file_to_package(data_source_id: str):
 
     use_case = AddFileToPackageUseCase(document_repository=document_repository, package_repository=package_repository)
 
-    document = Document.from_dict(request_data["document"])
-    added_document = use_case.execute(request_data["parentId"], document)
+    request_object = AddFileToPackageRequestObject.from_dict(request_data)
+
+    response = use_case.execute(request_object)
 
     return Response(
-        json.dumps(added_document.to_dict(), cls=DocumentSerializer), mimetype="application/json", status=200
+        json.dumps(response.value.to_dict(), cls=DocumentSerializer),
+        mimetype="application/json",
+        status=STATUS_CODES[response.type],
     )
 
 
@@ -41,6 +51,4 @@ def add_package_to_package(data_source_id: str):
     sub_package = SubPackage().from_dict(request_data["document"])
     added_sub_package = use_case.execute(request_data["parentId"], sub_package)
 
-    return Response(
-        json.dumps(added_sub_package.to_dict(), cls=DocumentSerializer), mimetype="application/json", status=200
-    )
+    return Response(json.dumps(added_sub_package.to_dict(), cls=DocumentSerializer), mimetype="application/json")
--- api/core/use_case/add_file_to_package_use_case.py
+++ api/core/use_case/add_file_to_package_use_case.py
@@ -1,17 +1,46 @@
 from core.domain.document import Document
 from core.domain.package import SubPackage
 from utils.logging import logger
+from core.shared import use_case as uc
+from core.shared import response_object as res
+from core.shared import request_object as req
 
 
-class AddFileToPackageUseCase:
+class AddFileToPackageRequestObject(req.ValidRequestObject):
+    def __init__(self, parent_id=None, document=None):
+        self.parent_id = parent_id
+        self.document = document
+
+    @classmethod
+    def from_dict(cls, adict):
+        invalid_req = req.InvalidRequestObject()
+
+        if "parentId" not in adict:
+            invalid_req.add_error("parentId", "is missing")
+
+        if "document" not in adict:
+            invalid_req.add_error("document", "is missing")
+
+        if invalid_req.has_errors():
+            return invalid_req
+
+        return AddFileToPackageRequestObject(
+            parent_id=adict.get("parentId"), document=Document.from_dict(adict.get("document"))
+        )
+
+
+class AddFileToPackageUseCase(uc.UseCase):
     def __init__(self, document_repository, package_repository):
         self.document_repository = document_repository
         self.package_repository = package_repository
 
-    def execute(self, parent_id: str, document: Document) -> Document:
+    def process_request(self, request_object):
+        parent_id = request_object.parent_id
+        document = request_object.document
+
         package: SubPackage = self.package_repository.get_by_id(parent_id)
         document_id = package.add_file(document.meta.name)
         self.package_repository.update(parent_id, package)
         document = self.document_repository.save(document, document_id)
         logger.info(f"Added document '{document_id}' to package '{parent_id}'")
-        return document
+        return res.ResponseSuccess(document)
