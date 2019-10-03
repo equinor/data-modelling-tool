a55a036751c092908d0a9905b2ed4e6e64054345
cbuv <cbuv@equinor.com>
2019-10-03 12:24:34 +0200

    pass bdd tests

--- api/tests_bdd/steps/schemas.py
+++ api/tests_bdd/steps/schemas.py
@@ -1,9 +1,7 @@
 from behave import given
-from app import init_import_internal, import_documents
-from services.database import data_modelling_tool_db
+from app import init_import_internal
 
 
 @given("data modelling tool templates are imported")
 def step_impl(context):
     init_import_internal("templates")
-    import_documents("dmt-templates", data_modelling_tool_db)
