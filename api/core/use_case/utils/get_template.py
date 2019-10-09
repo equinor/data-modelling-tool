88d56030d6073ce5a61714a608bb4631cfeb791a
cbuv <cbuv@equinor.com>
2019-10-09 10:15:54 +0200

    add cache on get_template #206

--- api/core/use_case/utils/get_template.py
+++ api/core/use_case/utils/get_template.py
@@ -1,7 +1,9 @@
 from core.domain.blueprint import Blueprint
 from core.repository.template_repository import get_template_by_document_type
+from functools import lru_cache
 
 
+@lru_cache(maxsize=32)
 def get_blueprint(type: str) -> Blueprint:
     DTO = get_template_by_document_type(type)
     data = DTO.data
