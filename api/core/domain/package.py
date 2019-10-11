3138a3c3207e467ec854394851d0e84a9c28c6bf
Stig Ofstad <stigofstad@gmail.com>
2019-10-11 10:52:08 +0200

    package dependencies

--- api/core/domain/package.py
+++ api/core/domain/package.py
@@ -1,4 +1,28 @@
-from typing import Dict, List, Optional
+from typing import Dict, List
+
+from config import Config
+
+
+class Dependency:
+    # TODO: Prod standard data_source as default ie. npm.org
+    # TODO: Enforce a semver system on version
+    def __init__(self, package: str, version: str = "latest", data_source: str = "templates", alias: str = None):
+        self.package = package
+        self.version = version
+        self.data_source = data_source
+        self.alias = alias
+
+    @classmethod
+    def from_dict(cls, a_dict):
+        return cls(
+            package=a_dict["package"],
+            version=a_dict["version"],
+            data_source=a_dict["data_source"],
+            alias=a_dict["alias"],
+        )
+
+    def to_dict(self):
+        return {"package": self.package, "version": self.version, "data_source": self.data_source, "alias": self.alias}
 
 
 class Package:
@@ -6,9 +30,10 @@ class Package:
         self,
         name: str,
         uid: str,
-        dependencies: Optional[List[Dict]] = None,
-        description: str = "",
-        type: str = "templates/DMT/Package",
+        dependencies: List[Dependency] = None,
+        description: str = None,
+        type: str = Config.DMT_PACKAGE,
+        # TODO: Should handle refs and contained blueprints to be consistent with rest of the system
         blueprints: List[Dict] = None,
         is_root: bool = False,
     ):
@@ -16,7 +41,6 @@ class Package:
         self.uid = uid
         self.description = description
         self.type = type
-        # TODO: Create Dependencies class
         self.dependencies = [] if not dependencies else dependencies
         self.blueprints = [] if not blueprints else blueprints
         self.packages = []
@@ -26,12 +50,12 @@ class Package:
     @classmethod
     def from_dict(cls, adict):
         instance = cls(
-            name=adict.get("name", ""),
-            uid=adict.get("_id", adict.get("uid")),
-            description=adict.get("description", ""),
-            blueprints=adict.get("blueprints", ""),
-            dependencies=adict.get("dependencies", ""),
-            type=adict.get("type", ""),
+            name=adict["name"],
+            uid=adict["_id"],
+            description=adict.get("description"),
+            blueprints=adict.get("blueprints"),
+            dependencies=[Dependency.from_dict(dependency) for dependency in adict.get("dependencies", [])],
+            type=adict.get("type", Config.DMT_PACKAGE),
         )
 
         instance.packages = [Package.from_dict(package) for package in adict.get("packages", "")]
@@ -64,7 +88,7 @@ class Package:
             "description": self.description,
             "type": self.type,
             "blueprints": self.blueprints,
-            "dependencies": self.dependencies,
+            "dependencies": [dependency.to_dict() for dependency in self.dependencies],
             "packages": [self.contained_package_to_dict(package) for package in self.packages],
             "isRoot": self.is_root,
             "storageRecipes": self.storage_recipes,
