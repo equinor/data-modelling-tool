1a4cf6a4a864b907dd9f18a22445d32c092f2a8e
Eirik Ola Aksnes <eoaksnes@gmail.com>
2019-10-18 18:20:57 +0200

    fix test

--- api/tests_bdd/steps/schemas.py
+++ api/tests_bdd/steps/schemas.py
@@ -4,5 +4,5 @@ from app import import_folder, BLUEPRINT_PACKAGES
 
 @given("data modelling tool templates are imported")
 def step_impl(context):
-    import_folder(contained=True, folder=BLUEPRINT_PACKAGES[1], collection="template")
-    import_folder(contained=True, folder=BLUEPRINT_PACKAGES[2], collection="template")
+    import_folder(contained=True, folder=BLUEPRINT_PACKAGES[1], collection="templates")
+    import_folder(contained=True, folder=BLUEPRINT_PACKAGES[2], collection="templates")
