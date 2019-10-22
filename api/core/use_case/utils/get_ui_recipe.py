d038b9b344d4d5e8330625708ddc954a434f3bda
Sindre Nistad <snis@equinor.com>
2019-10-22 16:55:46 +0200

    fix: Deal with empty blueprint

--- api/core/use_case/utils/get_ui_recipe.py
+++ api/core/use_case/utils/get_ui_recipe.py
@@ -2,8 +2,7 @@ from core.domain.ui_recipe import UIRecipe, DefaultUIRecipe
 
 
 def get_ui_recipe(blueprint, ui_recipe_name) -> UIRecipe:
-    ui_recipe = blueprint.get_ui_recipe(ui_recipe_name)
-    if ui_recipe:
-        return UIRecipe(name=ui_recipe["name"], attributes=ui_recipe.get("attributes", []))
-    else:
-        return DefaultUIRecipe()
+    if blueprint is not None:
+        if ui_recipe := blueprint.get_ui_recipe(ui_recipe_name):
+            return UIRecipe(name=ui_recipe["name"], attributes=ui_recipe.get("attributes", []))
+    return DefaultUIRecipe()
