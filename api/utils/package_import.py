4e85fa6bf87cdf1548c5f01fbf40d0198d053bdc
Stig Ofstad <stigofstad@gmail.com>
2019-10-22 11:35:04 +0200

    fix: import ApplicationSetting

--- api/utils/package_import.py
+++ api/utils/package_import.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Tuple
 from uuid import uuid4
 
 from config import Config
@@ -12,20 +12,28 @@ from services.database import data_modelling_tool_db as dmt_db
 from utils.logging import logger
 
 
-def _add_documents(path, documents, collection) -> List[Dict]:
+def _add_documents(path, documents, collection) -> Tuple[List[Dict], List[Dict]]:
     docs = []
+    app_settings = []
     for file in documents:
         with open(f"{path}/{file}") as json_file:
             data = json.load(json_file)
         data["uid"] = str(uuid4())
+
         if data["type"] == SIMOS.BLUEPRINT.value:
             document = Blueprint.from_dict(data)
         else:
             # TODO: Good candidate to become a DTO
             document = Entity(data)
+
         dmt_db[collection].replace_one({"_id": document.uid}, document.to_dict(), upsert=True)
-        docs.append({"_id": document.uid, "name": document.name})
-    return docs
+
+        if data["type"] == SIMOS.APPLICATION.value:
+            app_settings.append({"_id": document.uid, "name": document.name})
+        else:
+            docs.append({"_id": document.uid, "name": document.name})
+
+    return docs, app_settings
 
 
 def import_package(
@@ -39,8 +47,9 @@ def import_package(
         is_root=is_root,
     )
 
-    package.documents = _add_documents(path, documents=next(os.walk(path))[2], collection=collection)
-
+    package.documents, package.applications = _add_documents(
+        path, documents=next(os.walk(path))[2], collection=collection
+    )
     for folder in next(os.walk(path))[1]:
         package.packages.append(
             import_package(
