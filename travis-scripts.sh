52963b4e5c1c94690fc7568d34c1444ca24a2757
Eirik Ola Aksnes <eoaksnes@gmail.com>
2019-11-05 17:09:19 +0100

    fix travis

--- travis-scripts.sh
+++ travis-scripts.sh
@@ -12,12 +12,19 @@ pull_web() {
   docker pull $WEB_IMAGE || true
 }
 
+pull_nginx() {
+  docker pull $NGINX_IMAGE || true
+}
+
 if [ "$1" = "pull-web" ]; then
   docker_login
   pull_web
 elif [ "$1" = "pull-api" ]; then
   docker_login
   pull_api
+elif [ "$1" = "pull-nginx" ]; then
+  docker_login
+  pull_nginx
 elif [ "$1" = "build-api-dev-image" ]; then
   docker_login
   pull_api
