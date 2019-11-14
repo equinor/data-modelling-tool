c5f0bdf12c8d1222ddd046058bd10e31988eae09
Stig Ofstad <stigofstad@gmail.com>
2019-11-14 10:47:03 +0100

    remove deprecated keywords in radixconfig. fix init.sh

--- api/init.sh
+++ api/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if [ ! -e /home/first-run-false ]; then
+if [ ! -e /code/home/first-run-false ]; then
   echo "Importing data"
   /code/reset-database.sh
-  touch /home/first-run-false
+  touch /code/home/first-run-false
 fi
 flask run --host=0.0.0.0
