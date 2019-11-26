c4a47b7bdbc04d76db33591b6dc7af017b58cc13
Stig Ofstad <stigofstad@gmail.com>
2019-11-26 13:42:05 +0100

    bug: Application actions ui_contained

--- api/config.py
+++ api/config.py
@@ -20,7 +20,7 @@ class Config:
     DMT_SETTINGS_FILE = f"{APPLICATION_HOME}/dmt_settings.json"
     ENTITY_SETTINGS_FILE = f"{APPLICATION_HOME}/settings.json"
     SYSTEM_FOLDERS = ["SIMOS", "DMT"]
-    DEMO_ENTITIES = ["CarPackage"]
+    DEMO_ENTITIES = os.environ.get("DEMO_ENTITIES").split(",")
     with open(DMT_SETTINGS_FILE) as json_file:
         DMT_APPLICATION_SETTINGS = json.load(json_file)
     with open(ENTITY_SETTINGS_FILE) as json_file:
