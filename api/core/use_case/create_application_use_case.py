c31dd780a3a4ab1acbb99ea3a5d6b96ee8e9bdda
Stig Ofstad <stigofstad@gmail.com>
2019-11-26 12:35:45 +0100

    document actions.js in exported file

--- api/core/use_case/create_application_use_case.py
+++ api/core/use_case/create_application_use_case.py
@@ -74,6 +74,73 @@ services:
 def generate_runnable_file(runnable_models):
     class_template = Template(
         """\
+// This file is tightly coupled with the settings.json file, which contain your application settings.
+// The settings file has 'actions', they look like this;
+//     {
+//       "name": "Calculate",
+//       "description": "Simulate 80kmh concrete crash. Fiat panda 2011.",
+//       "input": "SSR-DataSource/CarPackage/Car",
+//       "output": "SSR-DataSource/CarPackage/Result",
+//       "method": "run"
+//     },
+//
+// This action will be available on any entity of the type "SSR-DataSource/CarPackage/Car"(input).
+// The name given in "method" must be the name of a function in this file, as well as being in the "runnableMethods" object at the end of this file.
+// The main use case for this custom function is to call SIMOS calculations, and update result objects.
+// The properties that are passed to the function looks like this;
+//       type Input = {
+//         blueprint: string
+//         entity: any
+//         path: string
+//         id: string
+//       }
+//
+//       type Output = {
+//         blueprint: string
+//         entity: any
+//         path: string
+//         dataSource: string
+//         id: string
+//       }
+//
+//       updateDocument(output: Output): Function
+//
+// Current limitations and caveats;
+// * updateDocument is a callBack. That means that if the web-browser get's interrupted(refresh,closed, etc.) the callBack is lost.
+// * The API uses a strict type system, so if the output entity does NOT match the output blueprint, that attribute will not be updated.
+// * The API uses an "update" strategy when writing the output entity. This means that it merges the existing document with the provided output entity.
+// * The output object must be left intact, and posted on every updateDocument call. Everything besides the output.entity object should be considered "read-only".
+// Here are a few examples;
+//
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms))
+// }
+//
+// function myExternalSystemCall(input) {
+//   return {
+//     jobId: 'kk873ks',
+//     result: 123456,
+//     executionTime: '1233215.34234ms',
+//     progress: 100,
+//     status: 'done',
+//     message: 'job complete, no errors',
+//   }
+// }
+//
+// async function run({ input, output, updateDocument }) {
+//   let entity = {
+//     ...output.entity,
+//     // This is an invalid attribute. Will not get written to database.
+//     hallo: 'Hey',
+//   }
+//   updateDocument({ ...output, entity })
+//
+//   // If the browser is interrupted during this sleep, the rest of the function will NOT be executed.
+//   await sleep(10000)
+//   entity = { diameter: 346 }
+//   updateDocument({ ...output, entity })
+//   updateDocument({ ...output, entity: myExternalSystemCall(input) })
+// }
 
 {% for runnable_model in runnable_models %}
 const {{ runnable_model["method"] }} = async ({input, output, updateDocument}) => {
@@ -83,7 +150,7 @@ const {{ runnable_model["method"] }} = async ({input, output, updateDocument}) =
         
 const runnableMethods = {
 {% for runnable_model in runnable_models %}
-    {{ runnable_model["method"] }}
+    {{ runnable_model["method"] }},
 {% endfor %}
 }
 
