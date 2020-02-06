839fd673642e7b232d312b51f4eef1a7310151c1
cbuv <cbuv@equinor.com>
2020-02-06 17:15:31 +0100

    fix attribute widget boolean

--- web/src/plugins/form-rjsf-widgets/BooleanWidget.tsx
+++ web/src/plugins/form-rjsf-widgets/BooleanWidget.tsx
@@ -44,10 +44,7 @@ export const BooleanWidget = (props: BoolDefaultInput) => {
   const { onChange, attributeType, value } = props
   const onChangeBool = (inputValue: boolean) => {
     let newValue: string | boolean = inputValue
-    if (attributeType.type === 'string') {
-      newValue = inputValue + '' //cast to string
-    }
-    onChange(attributeType, newValue)
+    onChange(attributeType, newValue + "")
   }
 
   const booleanValue = getBooleanValue(value, attributeType)
