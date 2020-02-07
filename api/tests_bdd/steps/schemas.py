4be2a8670449b11d5c29e01737d56af674b28182
Stig Ofstad <stigofstad@gmail.com>
2020-02-07 14:14:32 +0100

    silent logs in BDD tests

--- api/tests_bdd/steps/schemas.py
+++ api/tests_bdd/steps/schemas.py
@@ -3,17 +3,18 @@ import json
 from behave import given
 from core.repository import Repository
 from core.repository.repository_factory import get_repository
+from utils.logging import logger
 from utils.package_import import import_package
 
 from classes.dto import DTO
 from config import Config
 
-
 @given("data modelling tool templates are imported")
 def step_impl(context):
     for folder in Config.SYSTEM_FOLDERS:
+        logger.setLevel("ERROR")
         import_package(f"{Config.APPLICATION_HOME}/core/{folder}", collection=Config.SYSTEM_COLLECTION, is_root=True)
-
+        logger.setLevel("INFO")
 
 @given('there exist document with id "{uid}" in data source "{data_source_id}"')
 def step_impl_2(context, uid: str, data_source_id: str):
