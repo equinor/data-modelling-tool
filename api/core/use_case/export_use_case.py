49128b3ddd4c4a132c8cc8cd6792be96a366483e
Stig Ofstad <stigofstad@gmail.com>
2020-03-17 11:49:36 +0100

    use TreeNode to zip

--- api/core/use_case/export_use_case.py
+++ api/core/use_case/export_use_case.py
@@ -1,22 +1,24 @@
 # flake8: noqa: F401
 
-from config import Config
+import io
+import json
+import os
+import pathlib
+import zipfile
+
+from jinja2 import Template
+
 from classes.dto import DTO
 from classes.storage_recipe import StorageRecipe
+from config import Config
+from core.enums import DMT
 from core.repository import Repository
 from core.repository.repository_exceptions import EntityNotFoundException
+from core.service.document_service import DocumentService
 from core.shared import request_object as req
 from core.shared import response_object as res
 from core.shared import use_case as uc
-import zipfile
-import io
-import pathlib
-import json
-import os
-
-from core.enums import DMT
 from core.use_case.create_application_use_case import zip_package
-from jinja2 import Template
 
 
 class ExportRequestObject(req.ValidRequestObject):
@@ -37,17 +39,11 @@ class ExportRequestObject(req.ValidRequestObject):
 
 
 class ExportUseCase(uc.UseCase):
-    def __init__(self, document_repository: Repository):
-        self.document_repository = document_repository
+    def __init__(self, repository_provider, data_source_id):
+        self.document_service = DocumentService(repository_provider=repository_provider)
+        self.data_source_id = data_source_id
 
     def process_request(self, request_object: ExportRequestObject):
         document_id: str = request_object.document_id
-
-        memory_file = io.BytesIO()
-        with zipfile.ZipFile(memory_file, mode="w") as zip_file:
-            root_package: DTO = self.document_repository.get(document_id)
-            zip_package(zip_file, root_package, self.document_repository, f"{root_package.name}/")
-
-        memory_file.seek(0)
-
+        memory_file = self.document_service.create_zip_export(self.data_source_id, document_id)
         return res.ResponseSuccess(memory_file)
