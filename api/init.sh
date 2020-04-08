ec947af9b4d8cf3032d10b5b9fa21dec5e5264d9
Eirik Ola Aksnes <eoaksnes@gmail.com>
2020-04-08 14:59:32 +0200

    wait

--- api/init.sh
+++ api/init.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 set -euo pipefail
 
+echo "FLASK_ENV: $FLASK_ENV"
+
+if [ "$FLASK_ENV" = 'testing' ] ; then
+  sleep 60
+fi
+
 if [ "$ENVIRONMENT" = 'local' ] && [ "$FLASK_ENV" = 'development' ] ; then
-    cd /dmss/
-    python setup.py install
-    cd /code/
-  else
-    pip install dmss-api==0.2.4
-  fi
+  cd /dmss/
+  python setup.py install
+  cd /code/
+else
+  pip install dmss-api==0.2.4
+fi
 
 if [ "$1" = 'api' ]; then
   if [ ! -e /code/home/first-run-false ] && [ "$ENVIRONMENT" = 'local' ]; then
