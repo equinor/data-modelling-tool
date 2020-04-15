cda3e65d48023f480fd5f1d07d2e694d5d0f2cc6
Stig Ofstad <stigofstad@gmail.com>
2020-04-15 20:49:01 +0200

    dmss-api install from poetry

--- api/init.sh
+++ api/init.sh
@@ -26,17 +26,9 @@ service_is_ready "DMSS" $DMSS_HOST $DMSS_PORT
 if [ "$ENVIRONMENT" = 'local' ] && [ "$FLASK_ENV" = 'development' ] ; then
   cd /dmss/
   python setup.py install
-  cd /code/
-else
-  pip install dmss-api==0.2.4
 fi
 
 if [ "$1" = 'api' ]; then
-#  if [ ! -e /code/home/first-run-false ] && [ "$ENVIRONMENT" = 'local' ]; then
-#    echo "Importing data"
-#    /code/reset-database.sh
-#    touch /code/home/first-run-false
-#  fi
   flask run --host=0.0.0.0
 else
   exec "$@"
