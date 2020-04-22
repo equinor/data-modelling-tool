equinor/data-modelling-tool

commit 13ae3c5376d21c574421316db3e2df88ac3e00f9

feat: May download packages (and blueprints) as Python

api/core/use_case/utils/set_index_context_menu.py
@@ -1,8 +1,8 @@
+from typing import Union
+
 from classes.tree_node import Node
 from core.enums import DMT, SIMOS
 from core.use_case.utils.generate_index_menu_actions import (
-    get_create_reference_menu_item,
-    get_create_root_package_menu_item,
     get_delete_menu_item,
     get_download_menu_action,
     get_dynamic_create_menu_item,
@@ -10,9 +10,14 @@ from core.use_case.utils.generate_index_menu_actions import (
     get_import_menu_item,
     get_rename_menu_action,
     get_runnable_menu_action,
+    get_create_root_package_menu_item,
+    get_create_reference_menu_item,
+    get_export_python_code_menu_item,
 )
-from core.use_case.utils.sort_menu_items import sort_menu_items
+from core.utility import BlueprintProvider
 from utils.group_by import group_by
+
+from core.use_case.utils.sort_menu_items import sort_menu_items
 
 
 def create_context_menu(node: Node, data_source_id: str, app_settings: dict):
@@ -41,17 +46,6 @@ def create_context_menu(node: Node, data_source_id: str, app_settings: dict):
                     )
                 )
         else:
-            # Add create entry for optional attributes (not for packages)
-            for empty_child in [child for child in node.children if child.is_empty() and not child.is_array()]:
-                create_new_menu_items.append(
-                    get_dynamic_create_menu_item(
-                        data_source_id=data_source_id,
-                        name=empty_child.name,
-                        type=empty_child.type,
-                        node_id=empty_child.node_id,
-                        label=f"Create {empty_child.name}",
-                    )
-                )
             if node.is_array():
                 # List nodes can always append entities of it's own type
                 create_new_menu_items.append(
@@ -125,6 +119,8 @@ def create_context_menu(node: Node, data_source_id: str, app_settings: dict):
                     data_source_id=data_source_id, document_id=node.node_id, is_package_content=is_package
                 )
             )
+        # Context menu: Export Python code
+        menu_items.append(get_export_python_code_menu_item(data_source_id=data_source_id, document_id=node.node_id,))
 
         if create_new_menu_items:
             menu_items.append({"label": "New", "menuItems": create_new_menu_items})

api/classes/dto.py
@@ -1,6 +1,5 @@
 from typing import Dict, Optional
 from uuid import uuid4
-from utils.logging import logger
 
 
 # For backwards compatibility, data is not a private property.

api/classes/schema.py
@@ -7,12 +7,12 @@ import zlib
 from collections.abc import Iterable
 from pathlib import Path
 from types import CodeType
-from typing import Any, Callable, Dict, List, Optional, TypeVar, Union
+from typing import Any, Callable, Dict, List, Optional, TypeVar, Union, Set
 
 import stringcase
 from jinja2 import Template
 
-from classes.data_source import DataSource
+from classes.dto import DTO
 from config import Config
 from core.repository import Repository
 
@@ -243,6 +243,26 @@ class Attribute:
         return self.__values__.get(name, default)
 
 
+class __Blueprint__(type):
+    """ Used only for type annotation """
+
+    __completed__: bool
+    __schema__: dict
+    type: Union[str, __Blueprint__]
+
+    @classmethod
+    def __code__(
+        mcs,
+        include_dependencies: bool = False,
+        format_code=False,
+        include_imports: bool = True,
+        include_code_generation: bool = False,
+        keep_dmt_imports: bool = False,
+        _included_dependencies: Optional[Set[__Blueprint__]] = None,
+    ) -> str:
+        ...
+
+
 class Attributes:
     def __init__(self):
         self._attributes: List[Attribute] = []
@@ -288,13 +308,17 @@ def compress(data: str) -> str:
     return str(compressed)
 
 
-def _get_dto() -> str:
+def get_dto() -> str:
     with open(Path(__file__).parent / "dto.py") as f:
         content = "\n".join(f.readlines())
     return content
 
 
-TypeMapping = Dict[str, type]
+def get_project_line_length() -> int:
+    return Config.PY_PROJECT["tool"]["black"]["line-length"]
+
+
+TypeMapping = Dict[str, __Blueprint__]
 
 
 class TypeCache:
@@ -303,7 +327,7 @@ class TypeCache:
         self._fleeting = fleeting or {}
         self._static = basic_types()
 
-    def __getitem__(self, item: str) -> type:
+    def __getitem__(self, item: str) -> __Blueprint__:
         if self._is_template_internal(item):
             return self._permanent[item]
         elif self._is_static(item):
@@ -311,7 +335,7 @@ class TypeCache:
         else:
             return self._fleeting[item]
 
-    def __setitem__(self, key: str, value: type):
+    def __setitem__(self, key: str, value: __Blueprint__):
         if self._is_template_internal(key):
             self._permanent[key] = value
         else:
@@ -342,11 +366,13 @@ class Factory:
         _create_instance: bool = False,
         dump_site: Optional[str] = None,
         read_from_file: bool = False,
+        template_repository_getter: Optional[Callable[[str], Repository]] = None,
     ):
         self._types = TypeCache(self._internal_types)
         self._template_repository = template_repository
         self._read_from_file = read_from_file
         self._create_instance = _create_instance
+        self._template_repository_getter = template_repository_getter
         self.dump_site = dump_site
         self.macros = [
             self.type_name,
@@ -373,7 +399,8 @@ class Factory:
             default_as_loadable_json,
             compress,
             self.get_dependencies,
-            _get_dto,
+            get_dto,
+            get_project_line_length,
         ]
         self.to_be_compiled = set()
 
@@ -387,9 +414,20 @@ class Factory:
         if self._read_from_file:
             return self._template_repository.find({"type": template_type})
         else:
-            data_source_id, *_, name = template_type.split("/")
-            repository = self._template_repository.__class__(get_client(DataSource(data_source_id)))
-            return repository.find(filter={"name": name}, raw=True)
+            try:
+                data_source_id, *_, name = template_type.split("/")
+                repository = self._template_repository_getter(data_source_id)
+            except ValueError:
+                name = template_type
+                repository = self._template_repository
+            data = repository.find(filter={"name": name})
+            if isinstance(data, DTO):
+                data = data.data
+                try:
+                    del data["_id"]
+                except KeyError:
+                    pass
+            return data
 
     # noinspection GrazieInspection
     def class_from_schema(self, schema) -> str:
@@ -711,7 +749,7 @@ class {{ schema.name }}(metaclass={{ get_name_of_metaclass(schema) }}):
     __imports: bytes = {{ compress(self.imports()) }}
     __definition: bytes = {{ compress(self.definition()) }}
     __dmt_dependencies: List[bytes] = [
-        {{ compress(_get_dto()) }},
+        {{ compress(get_dto()) }},
     ]
 
     @classmethod
@@ -759,11 +797,14 @@ class {{ schema.name }}(metaclass={{ get_name_of_metaclass(schema) }}):
         if remove_dto_imports:
             for import_ in [
                 "from classes.dto import DTO",
-            ]: 
+            ]:
                 definition = definition.replace(import_, "")
         if format_code:
             import black
-            definition = black.format_str(definition, line_length=black.DEFAULT_LINE_LENGTH)
+            config = black.FileMode(
+                line_length={{ get_project_line_length() }},
+            )
+            definition = black.format_str(definition, mode=config)
         return definition
 {% endblock %}
 
@@ -913,7 +954,7 @@ from classes.dto import DTO
             f" = {self.get_default_value(attr)}" if attr.optional or attr.has_default else ""
         )
 
-    def signature(self, attributes: List[Attribute]) -> str:
+    def signature(self, attributes: Attributes) -> str:
         return f"{', '.join(self.variable_annotation(attr) for attr in attributes)}"
 
     def cast_as(self, attr: Attribute, name: Optional[str] = None) -> str:
@@ -940,14 +981,14 @@ from classes.dto import DTO
                 return f"self.{get_name_of_list_class(attr)}({self.cast(attr, f'{counter_name}', dimension - 1)} for {counter_name} in {value_name})"
         return f"{self.cast_as(attr, name)}"
 
-    def compile(self, schema: Dict) -> type:
+    def compile(self, schema: Dict) -> __Blueprint__:
         definition = self.class_from_schema(schema)
         if self.dump_site is not None:
             with open(self.dump_site, "a+") as f:
                 f.write(remove_imports(definition))
         code: CodeType = compile(definition, f"<string/{schema['name']}>", "exec", optimize=1)
         exec(code)  # nosec
-        cls: type = locals()[schema["name"]]
+        cls: __Blueprint__ = locals()[schema["name"]]
         if cls.__name__ not in globals():
             globals()[cls.__name__] = cls
         return cls
@@ -959,7 +1000,7 @@ from classes.dto import DTO
 
     def create(
         self, template_type: str, _create_instance: bool = False, compile: bool = True, write_domain: bool = False
-    ):
+    ) -> Optional[__Blueprint__]:
         if write_domain:
             return self.write_domain(template_type)
         try:
@@ -973,10 +1014,23 @@ from classes.dto import DTO
                 self._create(template_type, _create_instance)
             return Template
 
+    def create_from_schema(self, schema: dict, template_type: str) -> __Blueprint__:
+        Template = self._create_from_schema(schema, template_type)
+        for template_type in self.to_be_compiled:
+            self._create(template_type)
+        return Template
+
     def _create(self, template_type: str, _create_instance: bool = False, compile: bool = True):
-        schema = snakify(self._get_schema(template_type))
+        return self._create_from_schema(self._get_schema(template_type), template_type, _create_instance, compile)
+
+    def _create_from_schema(
+        self, schema: dict, template_type: str, _create_instance: bool = False, compile: bool = True
+    ) -> __Blueprint__:
+        if template_type in self._types and (_cls := self._types[template_type]).__completed__:
+            return _cls
+        schema = snakify(schema)
         # Let at "dummy type" be available for others
-        _cls = type(schema["name"], (), snakify(schema))
+        _cls: __Blueprint__ = type(schema["name"], (), schema)
         _cls.__schema__ = schema
         _cls.__completed__ = False
         schema["__class__"] = _cls

api/config.py
@@ -2,6 +2,8 @@ import json
 import os
 from pathlib import Path
 
+import toml
+
 
 class Config:
     MONGO_USERNAME = os.getenv("MONGO_INITDB_ROOT_USERNAME", "maf")
@@ -23,6 +25,9 @@ class Config:
     ENTITY_SETTINGS_FILE = f"{APPLICATION_HOME}/settings.json"
     SYSTEM_FOLDERS = ["SIMOS", "DMT"]
     VERIFY_IMPORTS = os.getenv("DMT_VERIFY_IMPORTS", True)
+    PY_PROJECT_FILE = f"{Path(__file__).parent.absolute()}/pyproject.toml"
+    with open(PY_PROJECT_FILE) as toml_file:
+        PY_PROJECT = toml.load(toml_file)
     with open(DMT_SETTINGS_FILE) as json_file:
         DMT_APPLICATION_SETTINGS = json.load(json_file)
     with open(ENTITY_SETTINGS_FILE) as json_file:

api/core/rest/System.py
@@ -1,5 +1,6 @@
 import json
 
+from core.use_case.generate_python_code import GeneratePythonCodeUseCase, GeneratePythonCodeRequestObject
 from flask import Blueprint, Response, send_file
 
 from classes.data_source import DataSource
@@ -51,3 +52,21 @@ def get():
             mimetype="application/json",
             status=STATUS_CODES[res.ResponseFailure.SYSTEM_ERROR],
         )
+
+
+@blueprint.route("/api/v2/system/<string:data_source_id>/generate-python-code/<string:document_id>", methods=["GET"])
+def generate_python_code(data_source_id: str, document_id: str):
+    logger.info(f"Compiling the blueprint '{document_id}', in '{data_source_id}' to Python code")
+    document_repository = get_repository(data_source_id)
+    request_object = GeneratePythonCodeRequestObject.from_dict(
+        {"documentId": document_id, "dataSourceId": data_source_id,}
+    )
+    use_case = GeneratePythonCodeUseCase(document_repository, get_repository)
+    response = use_case.execute(request_object)
+
+    if response.type == res.ResponseSuccess.SUCCESS:
+        return send_file(
+            response.value, mimetype="application/zip", as_attachment=True, attachment_filename="generated-code.zip"
+        )
+    else:
+        return Response(json.dumps(response.value), mimetype="application/json", status=STATUS_CODES[response.type])

api/core/use_case/generate_python_code.py
@@ -1,0 +1,177 @@
+from __future__ import annotations
+
+from collections import defaultdict
+from typing import Callable, Union, Dict, Optional, List
+
+from classes.schema import Factory, get_dto
+from classes.dto import DTO
+from core.repository import Repository
+from core.repository.repository_exceptions import EntityNotFoundException
+from core.shared import request_object as req
+from core.shared import response_object as res
+from core.shared import use_case as uc
+import zipfile
+import io
+
+from core.enums import DMT
+
+
+class GeneratePythonCodeRequestObject(req.ValidRequestObject):
+    def __init__(self, document_id: str = None, data_source_id: str = None):
+        self.document_id = document_id
+        self.data_source_id = data_source_id
+
+    @classmethod
+    def from_dict(cls, adict):
+        invalid_req = req.InvalidRequestObject()
+
+        if "documentId" not in adict:
+            invalid_req.add_error("documentId", "is missing")
+
+        if "dataSourceId" not in adict:
+            invalid_req.add_error("dataSourceId", "is missing")
+
+        if invalid_req.has_errors():
+            return invalid_req
+
+        return cls(document_id=adict.get("documentId"), data_source_id=adict.get("dataSourceId"),)
+
+
+def is_package(blueprint: DTO):
+    return blueprint.type == DMT.PACKAGE.value
+
+
+def is_simple(attribute) -> bool:
+    if isinstance(attribute, dict):
+        attribute_type = attribute["attribute_type"]
+    else:
+        attribute_type = attribute.attribute_type
+    return attribute_type in ["string", "number", "integer", "boolean"]
+
+
+def get_stringcase() -> str:
+    import stringcase
+
+    with open(stringcase.__file__) as f:
+        content = "\n".join(f.readlines())
+    return content
+
+
+class GeneratePythonCodeUseCase(uc.UseCase):
+    def __init__(self, document_repository: Repository, repository_getter: Callable[[str], Repository]):
+        self.document_repository = document_repository
+        self.repository_getter = repository_getter
+        self._factory = Factory(self.document_repository, template_repository_getter=self.repository_getter)
+
+    def get_blueprints(self, blueprint: DTO, files: dict = None, prefix: str = "") -> Dict[str, type]:
+        if files is None:
+            files = {}
+        if "_id" in blueprint.data:
+            del blueprint.data["_id"]
+        name = blueprint.name
+        if prefix:
+            name = f"{prefix}/{name}"
+        if is_package(blueprint):
+            for reference in blueprint["content"]:
+                dto = self.document_repository.get(reference["_id"])
+                self.get_blueprints(dto, files, name)
+        else:
+            try:
+                blueprint = self.get(name)
+            except Exception:
+                blueprint = self.from_schema(blueprint.data, name)
+            files[name] = blueprint
+        return files
+
+    def get_dependency_graph(self, blueprint, data_source_id: str) -> Dict[str, set]:
+        blueprints = self.get_blueprints(blueprint, prefix=data_source_id)
+        paths = {}
+        dependencies = defaultdict(set)
+        for path, blueprint in blueprints.items():
+            paths[blueprint] = path
+        blueprints = list(blueprints.keys())
+        while len(blueprints) > 0:
+            template_type = blueprints.pop()
+            if template_type not in dependencies:
+                blueprint = self.get(template_type)
+                for dependency in self.get_dependencies(blueprint):
+                    if dependency not in dependencies:
+                        dependencies[template_type].add(dependency)
+                        blueprints.append(dependency)
+                        _blueprint = self.get(dependency)
+        for _blueprint, _dependencies in dependencies.items():
+            dependencies[_blueprint] = _dependencies - {_blueprint}
+
+        return dependencies
+
+    @staticmethod
+    def get_dependencies(blueprint) -> List[str]:
+        dependencies = [blueprint._type]
+        try:
+            for attribute in blueprint.attributes:
+                if not is_simple(attribute):
+                    dependencies.append(attribute.attribute_type)
+        except AttributeError:
+            pass
+        return dependencies
+
+    def process_request(self, request_object: GeneratePythonCodeRequestObject):
+        document_id: str = request_object.document_id
+        data_source_id: str = request_object.data_source_id
+
+        blueprint: DTO = self.document_repository.get(document_id)
+        if not blueprint:
+            raise EntityNotFoundException(uid=document_id)
+
+        memory_file = io.BytesIO()
+        prefix = "dmt"
+        with zipfile.ZipFile(memory_file, mode="w") as zip_file:
+
+            def _dump(file_name: str, data: Union[str, bytes], encoding: str = "UTF-8") -> None:
+                """ Borrowed from git.equinor.com/APS/GUI """
+                file_name = file_name.replace("-", "_")  # Ensure package names are 'import friendly'
+                with zip_file.open(file_name, "w") as f:
+                    if isinstance(data, str):
+                        data = data.encode(encoding)
+                    if not data or data[-1] != b"\n":
+                        data += b"\n"
+                    f.write(data)
+
+            def write(template_type, blueprint, include_dependencies: bool = False, format_code: bool = True) -> None:
+                path = f"{prefix}/{template_type}{'' if template_type.endswith('.py') else '.py'}"
+                if path not in zip_file.namelist():
+                    if isinstance(blueprint, str):
+                        content = blueprint
+                    else:
+                        content = blueprint.__code__(
+                            include_dependencies=include_dependencies, format_code=format_code
+                        )
+                    _dump(path, content)
+
+            # Export stringcase
+            write("stringcase.py", get_stringcase())
+            if is_package(blueprint):
+                # Export the DTO class
+                write("classes/dto.py", get_dto())
+                blueprints = self.get_dependency_graph(blueprint, data_source_id)
+                for template_type, dependencies in blueprints.items():
+                    write(template_type, blueprint=self.get(template_type))
+                    for dependency in dependencies:
+                        write(dependency, blueprint=self.get(dependency))
+            else:
+                _blueprint = self.from_schema(blueprint.data)
+                write(blueprint.name, _blueprint, include_dependencies=True)
+
+        memory_file.seek(0)
+
+        return res.ResponseSuccess(memory_file)
+
+    def from_schema(self, schema: dict, template_type: Optional[str] = None):
+        if template_type is None:
+            template_type = schema["name"]
+        if "_id" in schema:
+            del schema["_id"]
+        return self._factory.create_from_schema(schema, template_type)
+
+    def get(self, template_type):
+        return self._factory.create(template_type)

api/core/use_case/utils/generate_index_menu_actions.py
@@ -154,3 +154,14 @@ def get_import_menu_item(data_source_id: str, document_id: str, is_package_conte
             "prompt": {"title": "Export", "content": "Download the package"},
         },
     }
+
+
+def get_export_python_code_menu_item(data_source_id, document_id: str):
+    return {
+        "label": "Generate Python Code",
+        "action": "DOWNLOAD",
+        "data": {
+            "url": f"/api/v2/system/{data_source_id}/generate-python-code/{document_id}",
+            "prompt": {"title": "Export", "content": "Download generated Python code"},
+        },
+    }

api/pyproject.toml
@@ -14,6 +14,7 @@ jsonschema = "^3.0"
 stringcase = "^1.2"
 python-dotenv = "^0.10.3"
 jinja2 = "^2.11.1"
+black = {version = "^19.10-beta0", allow-prereleases = true}
 
 [tool.poetry.dev-dependencies]
 pydevd = "^1.6"
@@ -25,7 +26,6 @@ snapshottest = "0.5.1"
 deepdiff = "4.0.7"
 pre-commit = "^1.18"
 pygments = "2.4.1"
-black = {version = "^18.3-alpha.0", allow-prereleases = true}
 coverage = "^5.0.3"
 flake8 = "^3.7.9"
 

commit 7eae38b873a0389c601a159eade630e0df8ca814

fix regression caused by 13ae3c5376d21c574421316db3e2df88ac3e00f9

api/core/use_case/generate_index_use_case.py
@@ -96,10 +96,8 @@ def extend_index_with_node_tree(root: Union[Node, ListNode], data_source_id: str
             # Skip package's content node
             if node.parent and node.parent.type == DMT.PACKAGE.value:
                 continue
-
             if not is_visible(node):
                 continue
-
             if node.has_error:
                 index[node.node_id] = get_error_node(data_source_id, node)
             else:

api/core/use_case/utils/set_index_context_menu.py
@@ -58,6 +58,18 @@ def create_context_menu(node: Node, data_source_id: str, app_settings: dict):
                             data_source_id=data_source_id, type=node.type, node_id=node.node_id
                         )
                     )
+            else:
+                # Add create entry for optional attributes (not for packages)
+                for empty_child in [child for child in node.children if child.is_empty()]:
+                    create_new_menu_items.append(
+                        get_dynamic_create_menu_item(
+                            data_source_id=data_source_id,
+                            name=empty_child.name,
+                            type=empty_child.type,
+                            node_id=empty_child.node_id,
+                            label=f"Create {empty_child.name}",
+                        )
+                    )
         # Everything besides listNodes can be renamed. Could be supported in future.
         if not node.is_array():
             menu_items.append(