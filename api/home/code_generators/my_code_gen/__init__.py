9abc4a3088a0abfd7bb7e96f8a8c1b1b1e567a1d
Stig Ofstad <stigofstad@gmail.com>
2020-04-22 09:46:18 +0200

    fix return type from code-gen

--- api/home/code_generators/my_code_gen/__init__.py
+++ api/home/code_generators/my_code_gen/__init__.py
@@ -1,2 +1,11 @@
+import io
+import zipfile
+
+
 def main(list_of_blueprints):
-    return list_of_blueprints
+    memory_file = io.BytesIO()
+    with zipfile.ZipFile(memory_file, mode="w") as zip_file:
+        zip_file.writestr("python-code.py", str(list_of_blueprints))
+
+    memory_file.seek(0)
+    return memory_file
