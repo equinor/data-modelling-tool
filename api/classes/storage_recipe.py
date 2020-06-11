f6ca5c241a09a99d0fbd4535ed0185928a9c17a4
Sindre Nistad <snis@equinor.com>
2020-06-11 18:39:44 +0200

    fix: "description" is required

--- api/classes/storage_recipe.py
+++ api/classes/storage_recipe.py
@@ -40,10 +40,15 @@ class StorageAttribute:
 
 class StorageRecipe:
     def __init__(
-        self, name: str, attributes: List[Dict] = None, storageAffinity: str = StorageDataTypes.DEFAULT.value
+        self,
+        name: str,
+        attributes: List[Dict] = None,
+        storageAffinity: str = StorageDataTypes.DEFAULT.value,
+        description: str = "",
     ):
         attributes = attributes if attributes else []
         self.name = name
+        self.description = description
         self.storage_affinity = StorageDataTypes(storageAffinity)
         self.storage_attributes = {attribute["name"]: StorageAttribute(**attribute) for attribute in attributes}
 
@@ -58,6 +63,7 @@ class StorageRecipe:
     def to_dict(self) -> Dict:
         return {
             "name": self.name,
+            "description": self.description,
             "storageAffinity": self.storage_affinity.value,
             "attributes": [attribute.to_dict() for attribute in self.storage_attributes.values()],
         }
