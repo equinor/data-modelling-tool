0e16964bde2d036115050751a4fcca2fffcd59ae
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2021-07-05 16:04:20 +0200

    add cache back to code

--- web/app/src/hooks/useExplorer.tsx
+++ web/app/src/hooks/useExplorer.tsx
@@ -189,8 +189,7 @@ export default function useExplorer(props: ExplorerProps): IUseExplorer {
   // TODO: This cache does not really work, as a new instance of useExplorer is created in every form
   const getBlueprint = (typeRef: string) => {
     // Check if blueprint is in cache
-    if (false) {
-      //cache not working in example.
+    if (typeRef in blueprintCache) {
       console.log(`Found ${typeRef} in cache!`)
       return blueprintCache[typeRef]
     } else {
