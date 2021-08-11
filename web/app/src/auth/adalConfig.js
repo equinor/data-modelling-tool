030ad56e8220be792fc883dd441cf3ffa855fe17
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2021-08-11 15:42:22 +0200

    working with requests

--- web/app/src/auth/adalConfig.js
+++ web/app/src/auth/adalConfig.js
@@ -4,7 +4,8 @@ export const adalConfig = {
   tenant: process.env.REACT_APP_AUTH_TENANT,
   clientId: process.env.REACT_APP_AUTH_CLIENT_ID,
   endpoints: {
-    api: process.env.REACT_APP_AUTH_CLIENT_ID,
+    auth_endpoint: process.env.REACT_APP_AUTH_ENDPOINT,
+    token_endpoint: process.env.REACT_APP_TOKEN_ENDPOINT
   },
   cacheLocation: 'localStorage',
 }
