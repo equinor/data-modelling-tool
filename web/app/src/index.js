22cef75ce8965064aff69b42555c71f48f7e5ea6
netr0m <MOAMU@equinor.com>
2021-11-15 16:06:09 +0100

    refactor to use AuthProvider and AuthContext from react-oauth2-code-pkce

--- web/app/src/index.js
+++ web/app/src/index.js
@@ -4,13 +4,28 @@ import 'react-notifications/lib/notifications.css'
 
 import App from './App'
 import ReactDOM from 'react-dom'
-import { AuthProvider } from '@dmt/common'
+import { AuthProvider } from 'react-oauth2-code-pkce'
 
 const authEnabled = process.env.REACT_APP_AUTH === '1'
+const authConfig = {
+  clientId: process.env.REACT_APP_AUTH_CLIENT_ID || '',
+  authorizationEndpoint: process.env.REACT_APP_AUTH_ENDPOINT || '',
+  tokenEndpoint: process.env.REACT_APP_TOKEN_ENDPOINT || '',
+  scope: process.env.REACT_APP_AUTH_SCOPE + ' openid' || '',
+  redirectUri: process.env.REACT_APP_AUTH_REDIRECT_URI || '',
+  logoutEndpoint: process.env.REACT_APP_LOGOUT_ENDPOINT || '',
+}
 
+// TODO: Discuss how to handle authEnabled=False
 ReactDOM.render(
-  <AuthProvider authEnabled={authEnabled}>
-    <App />,
-  </AuthProvider>,
+  <>
+    {authEnabled ? (
+      <AuthProvider authConfig={authConfig}>
+        <App />,
+      </AuthProvider>
+    ) : (
+      <App />
+    )}
+  </>,
   document.getElementById('root')
 )
