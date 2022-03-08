66740f17c7938074560c62bc30e8c6a1fdb1b61a
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2022-03-08 09:02:46 +0100

    prettier

--- web/packages/plugins/apps/analysis-platform/src/components/DynamicTable.tsx
+++ web/packages/plugins/apps/analysis-platform/src/components/DynamicTable.tsx
@@ -5,7 +5,7 @@ type Column = { name: string; accessor: string }
 
 const prepareColumns = (columns: Array<string>): Array<Column> => {
   const prepared: Array<Column> = []
-  columns.forEach(col => {
+  columns.forEach((col) => {
     prepared.push({ name: col, accessor: col.replace(/ /g, '') })
   })
 
@@ -25,16 +25,16 @@ const DynamicTable = (props: {
       <Table style={{ width: '100%' }}>
         <Table.Head sticky>
           <Table.Row>
-            {cols.map(col => (
+            {cols.map((col) => (
               <Table.Cell key={`head-${col.accessor}`}>{col.name}</Table.Cell>
             ))}
           </Table.Row>
         </Table.Head>
         <Table.Body onClick={onRowClicked} style={{ cursor: 'pointer' }}>
-          {rows?.map(row => (
+          {rows?.map((row) => (
             <Table.Row key={row._id} accessKey={row._id}>
               {Object.keys(row)
-                .filter(key => key !== '_id')
+                .filter((key) => key !== '_id')
                 .map((attrKey: string) => (
                   <Table.Cell key={attrKey}>{row[attrKey]}</Table.Cell>
                 ))}
