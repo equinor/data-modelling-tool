305bc351f8bb8aaea0506cdcfb3899e96d8d0c70
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2022-03-29 15:42:25 +0200

    run container job in azure

--- api/src/home/workflow/job_handlers/local_container/__init__.py
+++ api/src/home/workflow/job_handlers/local_container/__init__.py
@@ -6,7 +6,7 @@ from config import config
 from services.job_handler_interface import JobHandlerInterface, JobStatus
 from utils.logging import logger
 
-import os
+
 
 _SUPPORTED_TYPE = "WorkflowDS/Blueprints/jobHandlers/Container"
 
