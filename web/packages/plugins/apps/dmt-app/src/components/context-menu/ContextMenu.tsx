92be0b83c66289be65780099d7cd9695186a6f58
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2022-04-08 15:55:52 +0200

    wip

--- web/packages/plugins/apps/dmt-app/src/components/context-menu/ContextMenu.tsx
+++ web/packages/plugins/apps/dmt-app/src/components/context-menu/ContextMenu.tsx
@@ -1,11 +1,25 @@
-import React, { useContext, useState } from 'react'
-import { Button, Dialog, Scrim } from '@equinor/eds-core-react'
+import React, { useContext, useEffect, useState } from 'react'
+import {
+  Button,
+  Dialog,
+  Scrim,
+  Typography,
+  Input,
+  Label,
+} from '@equinor/eds-core-react'
 import './react-contextmenu.css'
 import { ContextMenu, ContextMenuTrigger, MenuItem } from 'react-contextmenu'
-import { AuthContext, BlueprintEnum, DmssAPI, TreeNode } from '@dmt/common'
+import {
+  AuthContext,
+  BlueprintEnum,
+  DmssAPI,
+  Select,
+  TreeNode,
+} from '@dmt/common'
 
 // @ts-ignore
 import { NotificationManager } from 'react-notifications'
+// import { CustomScrim } from '../../../../analysis-platform/src/components/CustomScrim'
 
 function sortMenuItems(menuItems: JSX.Element[]) {}
 
@@ -14,7 +28,9 @@ function createMenuItems(
   dmssAPI: DmssAPI,
   removeNode: Function,
   setShowScrim: Function,
-  setScrimContent: Function
+  setScrimContent: Function,
+  setFormData: Function,
+  getFormData: Function
 ): JSX.Element[] {
   let menuItems = []
 
@@ -38,6 +54,36 @@ function createMenuItems(
     window.open(`dmt/view/${node.nodeId}`, '_blank').focus()
   }
 
+  const NewFolderAction = (node: TreeNode) => {
+    // dmssAPI.createDocument()
+    const newPackage = {
+      name: 'test',
+      type: 'system/SIMOS/Package',
+      contained: true,
+    }
+    const ref = `${node.nodeId}.content.${node.entity.content.length}`
+    // dmssAPI.addDocumentToParent()
+    setScrimContent(
+      <div>
+        <Typography variant="h5">Create new folder</Typography>
+        <Label label="Folder name" />
+        <Input
+          type={'string'}
+          onChange={event => setFormData(event.target.value)}
+        />
+        <Button
+          onClick={() => {
+            console.log('submit', getFormData())
+          }}
+        >
+          Create
+        </Button>
+      </div>
+    )
+    setShowScrim(true)
+    return
+  }
+
   // dataSources get a "new root package"
   if (node.type === 'dataSource') {
     menuItems.push(
@@ -50,7 +96,7 @@ function createMenuItems(
   // Packages get a "new folder"
   if (node.type == BlueprintEnum.PACKAGE) {
     menuItems.push(
-      <MenuItem key={'new-package'} onClick={() => {}}>
+      <MenuItem key={'new-package'} onClick={() => NewFolderAction(node)}>
         New folder
       </MenuItem>
     )
@@ -116,14 +162,24 @@ export const NodeRightClickMenu = (props: {
   const { token } = useContext(AuthContext)
   const dmssAPI = new DmssAPI(token)
   const [showScrim, setShowScrim] = useState<boolean>(false)
+  const [formData, setFormData] = useState<any>()
   const [scrimContent, setScrimContent] = useState<JSX.Element>(<></>)
+  const getFormData = () => {
+    return formData
+  }
   const menuItems = createMenuItems(
     node,
     dmssAPI,
     removeNode,
     setShowScrim,
-    setScrimContent
+    setScrimContent,
+    setFormData,
+    getFormData
   )
+
+  useEffect(() => {
+    console.log('fromdata', formData)
+  }, [formData])
 
   return (
     <div>
