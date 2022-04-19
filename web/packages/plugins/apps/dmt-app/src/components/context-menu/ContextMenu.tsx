6557f003081119c69396e7d4e1644e0a1392da88
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2022-04-19 08:34:11 +0200

    temp

--- web/packages/plugins/apps/dmt-app/src/components/context-menu/ContextMenu.tsx
+++ web/packages/plugins/apps/dmt-app/src/components/context-menu/ContextMenu.tsx
@@ -16,21 +16,30 @@ import {
   Select,
   TreeNode,
 } from '@dmt/common'
-
+import { useCallback } from 'react'
 // @ts-ignore
 import { NotificationManager } from 'react-notifications'
 // import { CustomScrim } from '../../../../analysis-platform/src/components/CustomScrim'
 
 function sortMenuItems(menuItems: JSX.Element[]) {}
 
+function useTextInput() {
+  const [value, setValue] = useState<string>('')
+  console.log('val in usetextinput', value)
+  const input = <input onChange={e => setValue(e.target.value)} type={'text'} />
+  return [value, input]
+}
+
 function createMenuItems(
   node: TreeNode,
   dmssAPI: DmssAPI,
   removeNode: Function,
   setShowScrim: Function,
+  setShowScrimId: (id: string) => void,
   setScrimContent: Function,
-  setFormData: Function,
-  getFormData: Function
+  setFormData: (formData: any) => void,
+  formData: any,
+  getFormData?: () => string
 ): JSX.Element[] {
   let menuItems = []
 
@@ -54,36 +63,6 @@ function createMenuItems(
     window.open(`dmt/view/${node.nodeId}`, '_blank').focus()
   }
 
-  const NewFolderAction = (node: TreeNode) => {
-    // dmssAPI.createDocument()
-    const newPackage = {
-      name: 'test',
-      type: 'system/SIMOS/Package',
-      contained: true,
-    }
-    const ref = `${node.nodeId}.content.${node.entity.content.length}`
-    // dmssAPI.addDocumentToParent()
-    setScrimContent(
-      <div>
-        <Typography variant="h5">Create new folder</Typography>
-        <Label label="Folder name" />
-        <Input
-          type={'string'}
-          onChange={event => setFormData(event.target.value)}
-        />
-        <Button
-          onClick={() => {
-            console.log('submit', getFormData())
-          }}
-        >
-          Create
-        </Button>
-      </div>
-    )
-    setShowScrim(true)
-    return
-  }
-
   // dataSources get a "new root package"
   if (node.type === 'dataSource') {
     menuItems.push(
@@ -95,8 +74,46 @@ function createMenuItems(
 
   // Packages get a "new folder"
   if (node.type == BlueprintEnum.PACKAGE) {
+    const [folderName, userInput] = useTextInput()
+
+    const NewFolderAction = (node: TreeNode, folderName: string) => {
+      // dmssAPI.createDocument()
+      console.log('new folder action with node', node)
+      console.log('with name', folderName)
+      const newPackage = {
+        name: 'test',
+        type: 'system/SIMOS/Package',
+        contained: true,
+      }
+      // setShowScrim(false)
+    }
+
     menuItems.push(
-      <MenuItem key={'new-package'} onClick={() => NewFolderAction(node)}>
+      <MenuItem
+        key={'new-package'}
+        onClick={() => {
+          const ref = `${node.nodeId}.content.${node.entity.content.length}`
+          // dmssAPI.addDocumentToParent()
+          setScrimContent(
+            <div>
+              {}
+              <Typography variant="h5">Create new folder</Typography>
+              <Label label="Folder name" />
+
+              <Input
+                type={'string'}
+                onChange={event => setFormData(event.target.value)}
+              />
+              {userInput}
+              {/*@ts-ignore*/}
+              <Button onClick={() => NewFolderAction(node, folderName)}>
+                Create
+              </Button>
+            </div>
+          )
+          setShowScrim(true)
+        }}
+      >
         New folder
       </MenuItem>
     )
@@ -162,7 +179,8 @@ export const NodeRightClickMenu = (props: {
   const { token } = useContext(AuthContext)
   const dmssAPI = new DmssAPI(token)
   const [showScrim, setShowScrim] = useState<boolean>(false)
-  const [formData, setFormData] = useState<any>()
+  const [showScrimId, setShowScrimId] = useState<string>('')
+  const [formData, setFormData] = useState<any>('fack')
   const [scrimContent, setScrimContent] = useState<JSX.Element>(<></>)
   const getFormData = () => {
     return formData
@@ -172,20 +190,44 @@ export const NodeRightClickMenu = (props: {
     dmssAPI,
     removeNode,
     setShowScrim,
+    setShowScrimId,
     setScrimContent,
     setFormData,
+    formData,
     getFormData
   )
 
   useEffect(() => {
     console.log('fromdata', formData)
+    console.log('get form data', getFormData())
   }, [formData])
 
   return (
     <div>
-      {showScrim && <Scrim isDismissable>{scrimContent}</Scrim>}
+      {showScrim && (
+        <div>
+          <Scrim isDismissable>{scrimContent}</Scrim>
+        </div>
+      )}
       <ContextMenuTrigger id={node.nodeId}>{children}</ContextMenuTrigger>
       <ContextMenu id={node.nodeId}>{menuItems}</ContextMenu>
+      {/*{showScrimId === 'new-folder' && (*/}
+      {/*  <Scrim isDismissable>*/}
+      {/*    <div>*/}
+      {/*      <Typography variant="h5">Create new folder</Typography>*/}
+      {/*      <Label label="Folder name" />*/}
+      {/*      <Input*/}
+      {/*        type={'string'}*/}
+      {/*        onChange={event => setFormData(event.target.value)}*/}
+      {/*      />*/}
+      {/*      <Button onClick={() => console.log('create ', formData)}>*/}
+      {/*        Create*/}
+      {/*      </Button>*/}
+      {/*    </div>*/}
+      {/*  </Scrim>*/}
+      {/*)}*/}
     </div>
   )
 }
+
+// trying to set formData in NodeRightClickMenu and then returning it back into the created scrim is not working. have to refactor the whole component to make this work i think....
