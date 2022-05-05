f2bb5a895cd47c978af74028504d1e547fc8e5d8
netr0m <MOAMU@equinor.com>
2022-05-05 15:10:48 +0200

    change ref to old interface

--- web/packages/plugins/apps/dmt-app/src/context/dashboard/DashboardProvider.test.tsx
+++ web/packages/plugins/apps/dmt-app/src/context/dashboard/DashboardProvider.test.tsx
@@ -4,7 +4,7 @@ import DashboardProvider, { useDashboard } from './DashboardProvider'
 import {
   ApplicationContext,
   DataSources,
-  IDmssAPI,
+  DmssAPI,
   AuthProvider,
 } from '@dmt/common'
 import { mock } from 'jest-mock-extended'
@@ -23,7 +23,7 @@ describe('the dashboard provider component', () => {
   describe('when provider is initialized', () => {
     it('should correctly return the DashboardContext object', async () => {
       await act(async () => {
-        const api = mock<IDmssAPI>()
+        const api = mock<DmssAPI>()
         const { result, waitForNextUpdate } = renderHook(useDashboard, {
           wrapper,
           initialProps: {
@@ -43,7 +43,7 @@ describe('the dashboard provider component', () => {
     })
 
     it('should have fetched the index for all data sources', async () => {
-      const api = mock<IDmssAPI>()
+      const api = mock<DmssAPI>()
       const dataSources: DataSources = [
         {
           id: 'localhost',
