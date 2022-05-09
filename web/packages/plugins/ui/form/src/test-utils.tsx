ec2f0b00512705c3c3d5ca23be741c5b36ee5dab
netr0m <MOAMU@equinor.com>
2022-05-09 11:52:58 +0200

    fix blueprintGet mock function

--- web/packages/plugins/ui/form/src/test-utils.tsx
+++ web/packages/plugins/ui/form/src/test-utils.tsx
@@ -1,11 +1,14 @@
 import { DmssAPI } from '@dmt/common'
 
-export const mockGetBlueprint = (blueprints: any) => {
+export const mockBlueprintGet = (blueprints: Array<any>) => {
   const mock = jest.spyOn(DmssAPI.prototype, 'blueprintGet')
+  //@ts-ignore
   mock.mockImplementation((props: any) =>
-    Promise.resolve(
-      blueprints.find((blueprint: any) => blueprint.name == props.typeRef)
-    )
+    Promise.resolve({
+      data: blueprints.find(
+        (blueprint: any) => blueprint.name == props.typeRef
+      ),
+    })
   )
   return mock
 }
