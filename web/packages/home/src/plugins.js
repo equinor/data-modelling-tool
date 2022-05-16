b4b51806fa50d2e816d17964177f6642cb8162e3
Stig Ofstad <stigofstad@gmail.com>
2022-05-16 15:10:06 +0200

    edit blueprint plugin

--- web/packages/home/src/plugins.js
+++ web/packages/home/src/plugins.js
@@ -1,4 +1,5 @@
 export default [
+  import('@dmt/blueprint'),
   import('@dmt/default-pdf'),
   import('@dmt/default-preview'),
   import('@dmt/yaml-view'),
--- web/packages/plugins/shared/common/src/components/Select.tsx
+++ web/packages/plugins/shared/common/src/components/Select.tsx
@@ -4,11 +4,11 @@ import { INPUT_FIELD_WIDTH } from '../utils/variables'
 export const Select = styled.select`
   position: relative;
   font-size: medium;
-  padding: 8px 16px;
+  padding: 6px 8px;
   border: 0;
   border-bottom: 2px solid #dbdbdb;
   cursor: pointer;
-  width: fit-content;
+  height: 34px;
   background-color: #f7f7f7;
   width: ${INPUT_FIELD_WIDTH};
 `
