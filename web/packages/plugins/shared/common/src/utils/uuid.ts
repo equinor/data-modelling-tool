264bac7220b1be4f965b3431fb2bae65cb485eb9
netr0m <MOAMU@equinor.com>
2022-06-03 10:37:25 +0200

    fix jest tests

--- web/packages/plugins/shared/common/src/utils/uuid.ts
+++ web/packages/plugins/shared/common/src/utils/uuid.ts
@@ -1,5 +1,6 @@
 export function poorMansUUID(length = 10) {
   const randomNumbers = new Uint8Array(length)
   window.crypto.getRandomValues(randomNumbers)
+  // @ts-ignore
   return randomNumbers.reduce((a, b) => a.toString() + b.toString()).toString()
 }
