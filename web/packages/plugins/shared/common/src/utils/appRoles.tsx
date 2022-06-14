848071ef8069938c83e4cf67c786997d20e4b6ea
netr0m <MOAMU@equinor.com>
2022-06-14 10:06:40 +0200

    reduce dupes in role defs; domain role check

--- web/packages/plugins/shared/common/src/utils/appRoles.tsx
+++ web/packages/plugins/shared/common/src/utils/appRoles.tsx
@@ -1,18 +1,17 @@
-export const APP_ROLES: string[] = [
-  'dmss-admin',
-  'inspector',
-  'operator',
-  'expert-operator',
-  'domain-expert',
-  'domain-developer',
-]
-export const EXPERT_ROLES: string[] = [
-  'expert-operator',
+export const DMSS_ADMIN_ROLE: string = 'dmss-admin'
+export const INSPECTOR_ROLE: string = 'inspector'
+// Domain roles
+export const DOMAIN_ROLES: string[] = [
   'domain-developer',
   'domain-expert',
-  'dmss-admin',
+  DMSS_ADMIN_ROLE,
 ]
-export const OPERATOR_ROLES: string[] = EXPERT_ROLES.concat(['operator'])
+// Expert roles: Includes domain roles
+export const EXPERT_ROLES: string[] = ['expert-operator', ...DOMAIN_ROLES]
+// Operator roles: Includes expert roles
+export const OPERATOR_ROLES: string[] = ['operator', ...EXPERT_ROLES]
+// All roles
+export const APP_ROLES: string[] = [INSPECTOR_ROLE, ...OPERATOR_ROLES]
 
 export function getRoles(tokenData: any): string[] {
   /**
@@ -37,7 +36,7 @@ export function hasExpertRole(tokenData: any): boolean {
   if (process.env.REACT_APP_AUTH !== '1') return true
   const roles = getRoles(tokenData)
   if (roles) {
-    // If any of the roles the user has in the the EXPERT_ROLES array, return TRUE
+    // If any of the roles the user has are in the EXPERT_ROLES array, return TRUE
     return roles.some((role: string) => EXPERT_ROLES.includes(role))
   }
   return false
@@ -52,8 +51,23 @@ export function hasOperatorRole(tokenData: any): boolean {
   if (process.env.REACT_APP_AUTH !== '1') return true
   const roles = getRoles(tokenData)
   if (roles) {
-    // If any of the roles the user has in the the OPERATOR_ROLES array, return TRUE
+    // If any of the roles the user has are in the OPERATOR_ROLES array, return TRUE
     return roles.some((role: string) => OPERATOR_ROLES.includes(role))
+  }
+  return false
+}
+
+export function hasDomainRole(tokenData: any): boolean {
+  /**
+   * Check whether the user's token has a 'domain' role
+   * Accounts for any impersonated roles
+   */
+  // Always return true if auth is not enabled
+  if (process.env.REACT_APP_AUTH !== '1') return true
+  const roles = getRoles(tokenData)
+  if (roles) {
+    // If any of the roles the user has are in the DOMAIN_ROLES array, return TRUE
+    return roles.some((role: string) => DOMAIN_ROLES.includes(role))
   }
   return false
 }
