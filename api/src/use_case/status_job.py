060929fb9c90102cb7fca3a749003f3e00d288df
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2022-07-27 15:45:32 +0200

    fix job api and use case executor

--- api/src/use_case/status_job.py
+++ api/src/use_case/status_job.py
@@ -1,10 +1,10 @@
-from restful import response_object as res
 from restful import use_case as uc
 from services.job_service import JobService
+from starlette.responses import JSONResponse
 
 
 class StatusJobUseCase(uc.UseCase):
     def process_request(self, request_object):
         job_service = JobService()
         status, log, message = job_service.status_job(request_object["job_id"])
-        return res.ResponseSuccess({"status": status.value, "log": log, "message": message})
+        return JSONResponse({"status": status.value, "log": log, "message": message})
