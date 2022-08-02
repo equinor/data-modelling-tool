d8d9d8068a4b07b693dda82ff936ff62cddea384
netr0m <MOAMU@equinor.com>
2022-08-02 14:34:59 +0200

    add link colors for future use

--- web/packages/plugins/apps/analysis-platform/src/components/Design/Colors.ts
+++ web/packages/plugins/apps/analysis-platform/src/components/Design/Colors.ts
@@ -7,3 +7,5 @@ export const backgroundColorLight = colors.ui.background__light.rgba
 export const primary = colors.infographic.primary__moss_green_100.rgba
 export const primaryGray = '#f7f7f7'
 export const lightGray = '#f6f6f6'
+export const linkPrimary = colors.interactive.primary__resting.rgba
+export const linkHoverPrimary = colors.interactive.primary__hover.rgba
