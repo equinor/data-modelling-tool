928faa69dd7f0f8a40739d45b7c8ea3de367174a
Kristian Kjerstad <kristian.kjerstad@bouvet.no>
2022-08-02 16:26:43 +0200

    update blueprints endpoint

--- api/src/controllers/blueprints.py
+++ api/src/controllers/blueprints.py
@@ -1,17 +1,15 @@
 from fastapi import APIRouter
-from use_case.blueprints.get_related_blueprints_use_case import (
-    GetRelatedBlueprintsRequestObject,
-    GetRelatedBlueprintsUseCase,
-)
+from use_case.blueprints.get_related_blueprints_use_case import get_related_blueprints_use_case
+from restful.responses import create_response
 from utils.logging import logger
+from starlette.responses import JSONResponse
 
 router = APIRouter(tags=["Blueprints"], prefix="/blueprints")
 
 
 # TODO seems like this is not used anymore
 @router.get("/{target:path}", operation_id="get_blueprints")
+@create_response(JSONResponse)
 def get(target: str):
     logger.info(f"Getting blueprints used by '{target}'")
-    use_case = GetRelatedBlueprintsUseCase()
-    request_object = GetRelatedBlueprintsRequestObject(blueprint=target)
-    return use_case.execute(request_object)
+    return get_related_blueprints_use_case(target_blueprint=target)
