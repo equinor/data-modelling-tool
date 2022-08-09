45793c7cd55e3bc6372ed59e8f48978ee50fd624
netr0m <MOAMU@equinor.com>
2022-08-09 16:38:47 +0200

    ui plugin for viewing an asset

--- web/packages/plugins/analysis/src/index.tsx
+++ web/packages/plugins/analysis/src/index.tsx
@@ -1,6 +1,7 @@
 import { DmtPlugin, DmtPluginType } from '@dmt/common'
 import { OperatorView } from './OperatorView'
 import { InspectorView } from './InspectorView'
+import { AssetView } from './AssetView'
 
 export const plugins: DmtPlugin[] = [
   {
@@ -12,5 +13,10 @@ export const plugins: DmtPlugin[] = [
     pluginName: 'view-analysis-operator',
     pluginType: DmtPluginType.UI,
     component: OperatorView,
+  },
+  {
+    pluginName: 'view-asset',
+    pluginType: DmtPluginType.UI,
+    component: AssetView,
   },
 ]
