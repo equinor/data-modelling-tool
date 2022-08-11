7393b466caca6f7f2d752b49787dce6df6fae6ee
Stig Ofstad <stigofstad@gmail.com>
2022-08-11 13:32:26 +0200

    bug: wrong basePath used for DmtAPI

--- web/packages/plugins/shared/common/src/services/api/DmtAPI.ts
+++ web/packages/plugins/shared/common/src/services/api/DmtAPI.ts
@@ -1,9 +1,8 @@
 import { Configuration, DMTApi } from './configs/gen-dmt'
 
 export class DmtAPI extends DMTApi {
-  constructor(token: string, basePath: string = '/dmt') {
+  constructor(token: string) {
     const DMTConfiguration = new Configuration({
-      basePath: basePath,
       accessToken: token,
     })
     super(DMTConfiguration)
