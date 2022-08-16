b8013a1f3084f1576d3b61e990269628c7636205
Eirik Ola Aksnes <eoaksnes@gmail.com>
2022-08-16 11:31:43 +0200

    refactor: remove forcast of response weather plots

--- web/packages/plugins/for-weather-plots/src/uuid.js
+++
@@ -1,5 +0,0 @@
-export function poorMansUUID() {
-  const randomNumbers = new Uint8Array(10)
-  window.crypto.getRandomValues(randomNumbers)
-  return randomNumbers.reduce((a, b) => a.toString() + b.toString()).toString()
-}
