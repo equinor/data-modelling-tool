a956a9e82865595ecc49277df9ba7c33ca4a4172
Rikke Bang Landmark <rlan@equinor.com>
2022-09-12 11:06:51 +0200

    test: removed unused code, added clear all mocks

--- web/packages/dmt-core/src/hooks/useDocument.test.tsx
+++ web/packages/dmt-core/src/hooks/useDocument.test.tsx
@@ -14,41 +14,39 @@ const searchHits = [
     description: 'Description2',
   },
 ]
-const topHits = [
-  {
-    _id: '3',
-    name: 'Analysis3',
-    description: 'Description3',
-  },
-  {
-    _id: '4',
-    name: 'Analysis4',
-    description: 'Description4',
-  },
-]
 
-describe('useDocument hook', () => {
-  it('should correctly return the document', async () => {
-    const mock = mockGetDocument([searchHits], false)
-    const { result, waitForNextUpdate } = renderHook(() => useDocument('', ''))
-
-    await waitForNextUpdate()
-    expect(result.current[0]).toEqual([searchHits])
-    expect(mock).toHaveBeenCalledTimes(1)
-    expect(mock).toHaveBeenCalledWith({
-      dataSourceId: '',
-      documentId: '',
-      depth: 1,
-    })
+describe('useDocumentHook', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
   })
-  it('return error message when getdocumentbyid fails', async () => {
-    const mock = mockGetDocument([searchHits], true)
-    const { result, waitForNextUpdate } = renderHook(() => useDocument('', ''))
 
-    await waitForNextUpdate()
-    expect(result.current[0]).toEqual(null)
-    expect(result.current[1]).toEqual(false)
-    expect(result.current[3]).toEqual('error')
-    expect(mock).toHaveBeenCalledTimes(1)
+  describe('useDocument hook', () => {
+    it('should correctly return the document', async () => {
+      const mock = mockGetDocument([searchHits], false)
+      const { result, waitForNextUpdate } = renderHook(() =>
+        useDocument('', '')
+      )
+
+      await waitForNextUpdate()
+      expect(result.current[0]).toEqual([searchHits])
+      expect(mock).toHaveBeenCalledTimes(1)
+      expect(mock).toHaveBeenCalledWith({
+        dataSourceId: '',
+        documentId: '',
+        depth: 1,
+      })
+    })
+    it('return error message when getdocumentbyid fails', async () => {
+      const mock = mockGetDocument([searchHits], true)
+      const { result, waitForNextUpdate } = renderHook(() =>
+        useDocument('', '')
+      )
+
+      await waitForNextUpdate()
+      expect(result.current[0]).toEqual(null)
+      expect(result.current[1]).toEqual(false)
+      expect(result.current[3]).toEqual('error')
+      expect(mock).toHaveBeenCalledTimes(1)
+    })
   })
 })
