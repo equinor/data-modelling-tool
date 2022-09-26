40af1eb1becd9e7d28c9f86a57526458f475dcfc
kristian <kristian.kjerstad@bouvet.no>
2022-09-26 13:17:20 +0200

    fix: remove unused code from app.py and config.py

--- api/src/app.py
+++ api/src/app.py
@@ -1,15 +1,8 @@
-import concurrent.futures
-import io
-import json
-import os
-import traceback
-from pathlib import Path
-from zipfile import ZipFile
 
 import click
 import emoji
 import uvicorn
-from dmss_api.exceptions import ApiException
+
 from fastapi import APIRouter, FastAPI, Security
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
@@ -22,9 +15,7 @@ from config import config
 from restful.responses import ErrorResponse
 from services.dmss import dmss_api
 from store_headers_middleware import StoreHeadersMiddleware
-from utils.create_application_utils import zip_all
-from utils.import_package import import_package_tree, package_tree_from_zip
-from utils.logging import logger
+
 
 prefix = "/api/v1"
 
@@ -38,17 +29,13 @@ def auth_with_jwt(jwt_token: str = Security(oauth2_scheme)):
 
 
 def create_app():
-    from features.blueprints import blueprints
-    from features.entity import entity
+
     from features.jobs import jobs
-    from features.system import system
+
 
     public_routes = APIRouter()
     authenticated_routes = APIRouter()
-    public_routes.include_router(blueprints.router)
-    public_routes.include_router(entity.router)
     authenticated_routes.include_router(jobs.router)
-    public_routes.include_router(system.router)
 
     app = FastAPI(title="Data Modelling Tool", description="API for Data Modeling Tool (DMT)")
     app.include_router(public_routes, prefix=prefix)
@@ -79,147 +66,6 @@ def cli(token: str):
     dmss_api.api_client.default_headers["Authorization"] = "Bearer " + token
 
 
-@cli.command()
-def remove_application():
-    logger.info("-------------- REMOVING OLD APPLICATION FILES ----------------")
-    logger.debug(f"Removing application specific files from the configured DMSS instance; {config.DMSS_API}")
-
-    def thread_function(settings: dict) -> bool:
-        for package in settings.get("packages", []):
-            data_source_alias, folder = package.split("/", 1)
-            actual_data_source = next(
-                (v for k, v in settings["dataSourceAliases"].items() if k == data_source_alias), data_source_alias
-            )
-            logger.info(f"Deleting package '{actual_data_source}/{folder}' from DMSS...")
-            try:
-                dmss_api.explorer_remove_by_path(actual_data_source, directory=folder)
-            except ApiException as error:
-                if error.status == 404:
-                    logger.warning(emoji.emojize(f":warning: Could not find '{folder}' in DMSS..."))
-                else:
-                    raise error
-        return True
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for result in executor.map(thread_function, config.APP_SETTINGS.values()):
-            logger.debug(result)
-    logger.info("-------------- DONE ----------------")
-
-
-@cli.command()
-@click.argument("src")
-@click.argument("dst")
-def reset_package(src, dst):
-    logger.info(f"-------------- RESETTING PACKAGE {dst} ----------------")
-    if not Path(src).is_dir():
-        raise ValueError(f"'{src}' is not a directory. Current working directory is '{os.getcwd()}'")
-    data_source, folder = dst.split("/", 1)
-    try:
-        dmss_api.explorer_remove_by_path(data_source, directory=folder)
-    except ApiException as error:
-        if error.status == 404:
-            logger.warning(emoji.emojize(f":warning: Could not find '{folder}' in DMSS..."))
-        else:
-            raise error
-
-    memory_file = io.BytesIO()
-    with ZipFile(memory_file, mode="w") as zip_file:
-        zip_all(
-            zip_file,
-            src,
-            real_name=folder,  # Use target package name as name in zip archive
-            write_folder=True,
-        )
-    memory_file.seek(0)
-
-    root_package = package_tree_from_zip(data_source, folder, memory_file)
-    import_package_tree(root_package, data_source)
-
-    logger.info("-------------- DONE ----------------")
-
-
-@cli.command()
-@click.argument("path")
-def import_data_source(path: str):
-    filename = Path(path).name
-    logger.info(f"-------------- IMPORTING DATA SOURCE {filename} ----------------")
-    with open(path) as file:
-        document = json.load(file)
-        try:
-            dmss_api.data_source_save(document["name"], data_source_request=document)
-            logger.info(f"Added data source '{document['name']}'")
-        except (ApiException, KeyError) as error:
-            if error.status == 400:
-                logger.warning(
-                    emoji.emojize(
-                        f":warning: Could not import data source '{filename}'. "
-                        "A data source with that name already exists"
-                    )
-                )
-            else:
-                raise ImportError(f"Failed to import data source '{filename}': {error}")
-    logger.debug("_____ DONE importing data source _____")
-
-
-@cli.command()
-@click.pass_context
-def init_application(context):
-    logger.info("-------------- IMPORTING PACKAGES ----------------")
-
-    def thread_function(settings: dict) -> None:
-        app_directory_name = Path(settings["fileLocation"]).parent.name
-        logger.debug(f"Importing data for app '{settings['name']}'")
-        logger.debug("_____ importing data sources _____")
-        ds_dir = f"{config.APPLICATION_HOME}/{app_directory_name}/{config.APPS_DATASOURCE_SUBFOLDER}/"
-        data_sources_to_import = []
-        try:
-            data_sources_to_import = os.listdir(ds_dir)
-        except FileNotFoundError:
-            logger.warning(
-                emoji.emojize(f":warning: No 'data_source' directory was found under '{ds_dir}'. Nothing to import...")
-            )
-
-        for filename in data_sources_to_import:
-            context.invoke(import_data_source, path=f"{ds_dir}{filename}")
-
-        logger.debug("_____ DONE importing data sources _____")
-
-        logger.debug(f"_____ importing blueprints and entities {tuple(settings.get('packages', []))}_____")
-        for package in settings.get("packages", []):
-            data_source_alias, folder = package.split("/", 1)
-            actual_data_source = settings["dataSourceAliases"].get(data_source_alias, data_source_alias)
-            memory_file = io.BytesIO()
-            with ZipFile(memory_file, mode="w") as zip_file:
-                zip_all(
-                    zip_file,
-                    f"{config.APPLICATION_HOME}/{app_directory_name}/data/{data_source_alias}/{folder}",
-                    write_folder=True,
-                )
-            memory_file.seek(0)
-
-            try:
-                root_package = package_tree_from_zip(actual_data_source, folder, memory_file)
-                # Import the package into the data source defined in _aliases_, or using the data_source folder name
-                import_package_tree(root_package, actual_data_source)
-            except ApiException as error:
-                raise ApiException(error.body)
-            except Exception as error:
-                traceback.print_exc()
-                raise Exception(f"Something went wrong trying to upload the package '{package}' to DMSS; {error}")
-        logger.debug(f"_____ DONE importing blueprints and entities {tuple(settings.get('packages', []))}_____")
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for result in executor.map(thread_function, config.APP_SETTINGS.values()):
-            logger.debug(result)
-    logger.info(emoji.emojize("-------------- DONE ---------------- :check_mark_button:"))
-
-
-@cli.command()
-@click.pass_context
-def reset_app(context):
-    context.invoke(remove_application)
-    context.invoke(init_application)
-    config.load_app_settings()
 
 
 @cli.command()
--- api/src/config.py
+++ api/src/config.py
@@ -5,51 +5,6 @@ from pathlib import Path
 from typing import Dict, List, Union
 
 
-def parse_alias_file(file_path: str) -> Dict[str, str]:
-    regex = r"^[=A-Za-z0-9_-]*$"
-    pattern = re.compile(regex)
-    result: dict = {}
-    try:
-        with open(file_path) as alias_file:
-            for line in alias_file.read().splitlines():
-                if line.lstrip()[0] == "#":  # Skip commented lines
-                    continue
-                if not re.search(pattern, line):
-                    raise ValueError(f"The alias file '{file_path}' is invalid. Invalid line '{line}'")
-                key, value = line.split("=", 1)
-                result[key] = value
-
-    except FileNotFoundError:
-        print("WARNING: No data source alias file found...")
-    return result
-
-
-def replace_aliases_in_settings(settings: dict) -> dict:
-    def replace_alias_in_reference(reference: Union[str, None]) -> Union[str, None]:
-        if not reference:
-            return
-        if reference[0] == "/":  # Don't replace relative references
-            return reference
-        reference_data_source = reference.split("/", 1)[0]
-        return reference.replace(reference_data_source, aliases.get(reference_data_source, reference_data_source), 1)
-
-    aliases = settings["dataSourceAliases"]
-
-    new_models: List[str] = []
-    for model in settings.get("models", []):
-        new_models.append(replace_alias_in_reference(model))
-
-    new_actions: List[dict] = []
-    for action in settings.get("actions", []):
-        action["input"] = replace_alias_in_reference(action.get("input"))
-        action["output"] = replace_alias_in_reference(action.get("output"))
-        new_actions.append(action)
-
-    settings["models"] = new_models
-    settings["actions"] = new_actions
-
-    return settings
-
 
 class Config:
     MONGO_USERNAME = os.getenv("MONGO_INITDB_ROOT_USERNAME", "maf")
@@ -63,7 +18,6 @@ class Config:
     DYNAMIC_MODELS = "dynamic_models"
     CACHE_MAX_SIZE = 200
     APPLICATION_HOME = os.getenv("APPLICATION_HOME", f"{Path(__file__).parent}/home")
-    DMT_ENTITIES_HOME = f"{Path(__file__).parent}/demo_app_home"
     VERIFY_IMPORTS = os.getenv("DMT_VERIFY_IMPORTS", False)
     DMSS_API = os.getenv("DMSS_API", "http://dmss:5000")
 
@@ -88,35 +42,5 @@ class Config:
     SCHEDULER_REDIS_PORT = int(os.getenv("SCHEDULER_REDIS_PORT", 6379))
     SCHEDULER_REDIS_SSL = os.getenv("SCHEDULER_REDIS_SSL", "False").lower() == "true"
 
-    APP_NAMES = next(os.walk(APPLICATION_HOME))[1]  # Every folder under HOME represents a separate app
-    APP_SETTINGS: Dict[str, dict] = {}  # Dict holding settings for all loaded applications
-    APPS_DATASOURCE_SUBFOLDER = "data_sources"
-
-    def load_app_settings(self):
-        for app in self.APP_NAMES:
-            try:
-                with open(f"{self.APPLICATION_HOME}/{app}/settings.json") as json_file:
-                    app_settings: dict = json.load(json_file)
-                    app_settings["fileLocation"] = json_file.name
-                    app_settings["dataSourceAliases"] = parse_alias_file(
-                        f"{self.APPLICATION_HOME}/{app}/data/_aliases_"
-                    )
-
-                    code_gen_folder = Path(f"{self.APPLICATION_HOME}/{app}/code_generators")
-                    if code_gen_folder.is_dir():
-                        app_settings["codeGenerators"] = os.listdir(str(code_gen_folder))
-
-                    self.APP_SETTINGS[app_settings["name"]] = replace_aliases_in_settings(app_settings)
-            except FileNotFoundError:
-                raise FileNotFoundError(
-                    f"No settings file found for the app '{app}'."
-                    f"Each application requires a 'settings.json' file located at "
-                    f"'{self.APPLICATION_HOME}/{{name-of-app}}/'"
-                )
-            except KeyError as e:
-                raise KeyError(f"The settings file for the '{app}' application is invalid: {e}")
-            print(f"Successfully loaded app '{app}'")
-
 
 config = Config()
-config.load_app_settings()
