32c77f78dcb4955f21696b9a604b9685bf509852
kristian <kristian.kjerstad@bouvet.no>
2022-09-26 13:08:09 +0200

    fix: remove unused API features

--- api/src/features/system/use_cases/create_application_use_case.py
+++
@@ -1,7 +0,0 @@
-from services.application_service import ApplicationService
-from services.document_service import DocumentService
-
-
-def create_application_use_case(data_source_id: str, application_id: str):
-    application_service = ApplicationService(DocumentService())
-    return application_service.create_application(data_source_id, application_id)
